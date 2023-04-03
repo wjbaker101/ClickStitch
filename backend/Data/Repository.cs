4e4ccbd6a3bee9a7da3aa6549a5cc5e24a31affa
wjbaker101 <willbaker10198@gmail.com>
2023-04-03 10:09:05 +0100

    Delete unused

--- backend/Data/Repository.cs
+++ backend/Data/Repository.cs
@@ -4,12 +4,6 @@ namespace Data;
 
 public interface IRepository<TRecord>  where TRecord : IDatabaseRecord
 {
-    TRecord Save(TRecord record);
-    List<TRecord> SaveMany(IEnumerable<TRecord> records);
-    TRecord Update(TRecord record);
-    List<TRecord> UpdateMany(IEnumerable<TRecord> records);
-    TRecord Delete(TRecord record);
-    List<TRecord> DeleteMany(IEnumerable<TRecord> records);
     Task<TRecord> SaveAsync(TRecord record);
     Task<List<TRecord>> SaveManyAsync(IEnumerable<TRecord> records);
     Task<TRecord> UpdateAsync(TRecord record);
@@ -25,87 +19,6 @@ public abstract class Repository<TRecord> : IRepository<TRecord> where TRecord :
     protected Repository(IDatabase database)
     {
         Database = database;
-    }
-
-    public TRecord Save(TRecord record)
-    {
-        using var session = Database.SessionFactory.OpenSession();
-        using var transaction = session.BeginTransaction();
-
-        session.Save(record);
-
-        transaction.Commit();
-
-        return record;
-    }
-
-    public List<TRecord> SaveMany(IEnumerable<TRecord> records)
-    {
-        using var session = Database.SessionFactory.OpenSession();
-        using var transaction = session.BeginTransaction();
-
-        var recordsAsList = records.ToList();
-
-        foreach (var record in recordsAsList)
-            session.Save(record);
-
-        transaction.Commit();
-
-        return recordsAsList;
-    }
-
-    public TRecord Update(TRecord record)
-    {
-        using var session = Database.SessionFactory.OpenSession();
-        using var transaction = session.BeginTransaction();
-
-        session.Update(record);
-
-        transaction.Commit();
-
-        return record;
-    }
-
-    public List<TRecord> UpdateMany(IEnumerable<TRecord> records)
-    {
-        using var session = Database.SessionFactory.OpenSession();
-        using var transaction = session.BeginTransaction();
-
-        var recordsAsList = records.ToList();
-
-        foreach (var record in recordsAsList)
-            session.Update(record);
-
-        transaction.Commit();
-
-        return recordsAsList;
-    }
-
-    public TRecord Delete(TRecord record)
-    {
-        using var session = Database.SessionFactory.OpenSession();
-        using var transaction = session.BeginTransaction();
-
-        session.Delete(record);
-
-        transaction.Commit();
-
-        return record;
-    }
-
-    public List<TRecord> DeleteMany(IEnumerable<TRecord> records)
-    {
-        using var session = Database.SessionFactory.OpenSession();
-        using var transaction = session.BeginTransaction();
-
-        var recordsAsList = records.ToList();
-
-        foreach (var record in recordsAsList)
-            session.Delete(record);
-
-        transaction.Commit();
-
-        return recordsAsList;
     }
 
     public async Task<TRecord> SaveAsync(TRecord record)
