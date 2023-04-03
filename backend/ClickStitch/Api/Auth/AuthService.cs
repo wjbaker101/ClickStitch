864a61a5d9ad74b581a650cedc9a1a765c93a746
wjbaker101 <willbaker10198@gmail.com>
2023-04-03 17:58:28 +0100

    Rename api property

--- backend/ClickStitch/Api/Auth/AuthService.cs
+++ backend/ClickStitch/Api/Auth/AuthService.cs
@@ -25,7 +25,7 @@ public sealed class AuthService : IAuthService
 
     public async Task<Result<LogInResponse>> LogIn(LogInRequest request)
     {
-        var userResult = await _userRepository.GetByEmailAsync(request.Username);
+        var userResult = await _userRepository.GetByEmailAsync(request.Email);
         if (!userResult.TrySuccess(out var user))
             return Result<LogInResponse>.FromFailure(userResult);
 
--- backend/ClickStitch/Api/Auth/Types/LogIn.cs
+++ backend/ClickStitch/Api/Auth/Types/LogIn.cs
@@ -2,7 +2,7 @@
 
 public sealed class LogInRequest
 {
-    public required string Username { get; init; }
+    public required string Email { get; init; }
     public required string Password { get; init; }
 }
 
