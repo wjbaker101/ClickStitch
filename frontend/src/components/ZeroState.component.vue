145a5c812da63e179c944a7568562898cdb43ce7
wjbaker101 <willbaker10198@gmail.com>
2023-04-08 09:51:22 +0100

    Add component to convey info

---
+++ frontend/src/components/ZeroState.component.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="zero-state-component text-centered">
+        <IconComponent :icon="icon" size="huge" />
+        <slot></slot>
+    </div>
+</template>
+
+<script setup lang="ts">
+import IconComponent, { IconName } from '@wjb/vue/component/IconComponent.vue';
+
+defineProps<{
+    icon: IconName;
+}>();
+</script>
+
+<style lang="scss">
+.zero-state-component {}
+</style>
