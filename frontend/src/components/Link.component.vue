00935a254f61c6d34720cb77a52c5f0164b96a74
wjbaker101 <willbaker10198@gmail.com>
2023-05-02 21:26:59 +0100

    Create a link component with styling that supports underline for icons

---
+++ frontend/src/components/Link.component.vue
@@ -0,0 +1,25 @@
+<template>
+    <a class="link-component" :href="href" target="_blank">
+        <IconComponent v-if="external" icon="external-link" gap="right" />
+        <slot></slot>
+    </a>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+    href: string;
+    external?: boolean;
+}>();
+</script>
+
+<style lang="scss">
+.link-component {
+    text-decoration: none;
+    color: var(--wjb-primary-dark);
+    box-shadow: 0 1px 0 var(--wjb-primary-dark);
+
+    &:hover {
+        box-shadow: 0 1px 0 transparent;
+    }
+}
+</style>
--- frontend/src/setup/components.setup.ts
+++ frontend/src/setup/components.setup.ts
@@ -1,12 +1,14 @@
 import { Plugin } from 'vue';
 
 import CardComponent from '@/components/Card.component.vue';
+import LinkComponent from '@/components/Link.component.vue';
 import ViewComponent from '@/components/View.component.vue';
 
 export const appComponents: Plugin = {
 
     install(app) {
         app.component('CardComponent', CardComponent);
+        app.component('LinkComponent', LinkComponent);
         app.component('ViewComponent', ViewComponent);
     },
 };
