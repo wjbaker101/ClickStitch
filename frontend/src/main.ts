0fb9e23074c2d607d507ef947c1c61d4df80af5d
wjbaker101 <willbaker10198@gmail.com>
2023-05-03 22:30:21 +0100

    Merge components plugin

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -3,13 +3,11 @@ import { createApp } from 'vue';
 import App from '@/App.vue';
 
 import { router } from '@/setup/router.setup';
-import { appComponents } from '@/setup/components.setup';
-import { components } from '@wjb/vue/setup/components';
+import { components } from '@/setup/components.setup';
 
 const app = createApp(App);
 
 app.use(router);
-app.use(appComponents);
 app.use(components);
 
 app.mount('#app');
--- frontend/src/setup/components.setup.ts
+++ frontend/src/setup/components.setup.ts
@@ -1,12 +1,16 @@
 import { Plugin } from 'vue';
 
+import { components as wjbComponents } from '@wjb/vue/setup/components';
+
 import CardComponent from '@/components/Card.component.vue';
 import LinkComponent from '@/components/Link.component.vue';
 import ViewComponent from '@/components/View.component.vue';
 
-export const appComponents: Plugin = {
+export const components: Plugin = {
 
     install(app) {
+        app.use(wjbComponents);
+
         app.component('CardComponent', CardComponent);
         app.component('LinkComponent', LinkComponent);
         app.component('ViewComponent', ViewComponent);
