2b55f36eb931fd2926423496df695bfb2fffb0a8
wjbaker101 <willbaker10198@gmail.com>
2023-05-06 16:53:26 +0100

    Add migration for last_login_at column

---
+++ Migrations/Migrations/Migrations/0012 - AddLastLoginAtColumnToUserTable.cs
@@ -0,0 +1,27 @@
+﻿using FluentMigrator;
+using Migrations.Common;
+
+namespace Migrations.Migrations;
+
+// ReSharper disable once InconsistentNaming
+[Migration(0012)]
+public sealed class AddLastLoginAtColumnToUserTable_0012 : Migration
+{
+    public override void Up()
+    {
+        Create
+            .Column("last_login_at")
+            .OnTable(Names.Tables.USER)
+            .InSchema(Names.Schemas.CLICK_STITCH)
+            .AsDateTimeOffset()
+            .Nullable();
+    }
+
+    public override void Down()
+    {
+        Delete
+            .Column("last_login_at")
+            .FromTable(Names.Tables.USER)
+            .InSchema(Names.Schemas.CLICK_STITCH);
+    }
+}
