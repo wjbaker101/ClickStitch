797328cc9048545cc58d407974311a76d8b5bb31
wjbaker101 <willbaker10198@gmail.com>
2023-05-10 20:15:32 +0100

    Create optional user version

--- backend/ClickStitch/Helper/RequestHelper.cs
+++ backend/ClickStitch/Helper/RequestHelper.cs
@@ -4,8 +4,14 @@ public static class RequestHelper
 {
     public const string REQUEST_USER_ITEM_KEY = "user";
 
-    public static RequestUser GetRequiredUser(HttpRequest httpRequest)
+    public static RequestUser GetRequiredUser(HttpRequest httpRequest) => GetUser(httpRequest) ?? throw new Exception("User expected on the request but was not found.");
+    
+    public static RequestUser? GetOptionalUser(HttpRequest httpRequest) => GetUser(httpRequest);
+
+    private static RequestUser? GetUser(HttpRequest httpRequest)
     {
-        return httpRequest.HttpContext.Items[REQUEST_USER_ITEM_KEY] as RequestUser ?? throw new Exception("User expected on the request but was not found.");
+        var isUserPresent = httpRequest.HttpContext.Items.TryGetValue(REQUEST_USER_ITEM_KEY, out var user);
+
+        return isUserPresent ? user as RequestUser : null;
     }
 }
