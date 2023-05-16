1b72ea5d83ecc85d74eb3cf716e1dd86d9c89956
wjbaker101 <willbaker10198@gmail.com>
2023-05-16 21:40:49 +0100

    Retrieve the users on load

--- frontend/src/views/admin/Admin.view.vue
+++ frontend/src/views/admin/Admin.view.vue
@@ -7,6 +7,8 @@
             <section>
                 <CardComponent border="top" padded>
                     <h2>Users</h2>
+                    <LoadingComponent v-if="getUsers === null" />
+                    <div v-else></div>
                 </CardComponent>
             </section>
         </div>
@@ -14,6 +16,21 @@
 </template>
 
 <script setup lang="ts">
+import { onMounted, ref } from 'vue';
+
+import { api } from '@/api/api';
+
+import { IGetUsers } from '@/models/GetUsers.model';
+
+const getUsers = ref<IGetUsers | null>(null);
+
+onMounted(async () => {
+    const result = await api.admin.getUsers();
+    if (result instanceof Error)
+        return;
+
+    getUsers.value = result;
+});
 </script>
 
 <style lang="scss">
