059b6aa1cbd0cebd6e3eba5085cdd79991c21a29
wjbaker101 <willbaker10198@gmail.com>
2023-05-18 20:22:02 +0100

    Create test for manually doing test password stuff

---
+++ backend/Tests/Api.Tests/Miscellaneous/PasswordTests.cs
@@ -0,0 +1,24 @@
+﻿using ClickStitch.Api.Auth;
+
+namespace Api.Tests.Miscellaneous;
+
+[TestFixture]
+[Parallelizable]
+public sealed class GivenAPasswordAndSalt
+{
+    private string _result = null!;
+
+    [OneTimeSetUp]
+    public void Setup()
+    {
+        var subject = new PasswordService();
+
+        _result = subject.Hash("<password>", "<password_salt>");
+    }
+
+    [Test]
+    public void ThenTheHashedPasswordIsDisplayed()
+    {
+        Assert.Pass(_result);
+    }
+}
