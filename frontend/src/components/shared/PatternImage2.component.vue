34cac868df09a5924a1063a4696012168b0a9c14
wjbaker101 <willbaker10198@gmail.com>
2023-05-22 20:19:00 +0100

    Add new version of the pattern image that doesn't require the pattern

---
+++ frontend/src/components/shared/PatternImage2.component.vue
@@ -0,0 +1,32 @@
+<template>
+    <img width="544" height="363" class="pattern-image-component" :src="image ?? undefined" loading="lazy">
+</template>
+
+<script setup lang="ts">
+defineProps<{
+    image: string | null;
+}>();
+</script>
+
+<style lang="scss">
+@use '@/style/variables' as *;
+
+.pattern-image-component {
+    max-width: 100%;
+    height: auto;
+    border-radius: var(--wjb-border-radius);
+    vertical-align: middle;
+    animation: pattern-image-component-load 0.5s;
+
+    @include shadow-small();
+
+    @keyframes pattern-image-component-load {
+        from {
+            opacity: 0;
+        }
+        to {
+            opacity: 1;
+        }
+    }
+}
+</style>
