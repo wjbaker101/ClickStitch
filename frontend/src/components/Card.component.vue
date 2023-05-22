d57a9d6ab30726e9befb0674876dbc75883fdb87
wjbaker101 <willbaker10198@gmail.com>
2023-05-22 20:19:06 +0100

    Update card styling

--- frontend/src/components/Card.component.vue
+++ frontend/src/components/Card.component.vue
@@ -7,6 +7,9 @@
             'is-padded': padded,
         }"
     >
+        <header v-if="header">
+            <slot name="header"></slot>
+        </header>
         <slot></slot>
     </div>
 </template>
@@ -16,6 +19,7 @@ defineProps<{
     border?: 'top' | 'right' | 'bottom' | 'left';
     hoverable?: boolean;
     padded?: boolean;
+    header?: boolean;
 }>();
 </script>
 
@@ -29,8 +33,8 @@ defineProps<{
     border-color: var(--wjb-primary);
     outline: 2px dashed transparent;
     outline-offset: 1px;
-
-    @include shadow-small();
+    border-radius: var(--wjb-border-radius);
+    box-shadow: 0 2px 12px rgba(0, 0, 0, 0.1), 0 6px 16px -12px rgba(0, 0, 0, 1);
 
     &.is-hoverable {
         cursor: pointer;
@@ -74,6 +78,28 @@ defineProps<{
         border-left-width: 2px;
         border-top-right-radius: var(--wjb-border-radius);
         border-bottom-right-radius: var(--wjb-border-radius);
+    }
+
+    & > header {
+        margin: -1rem;
+        margin-bottom: 1rem;
+        background-color: var(--wjb-primary);
+        background: linear-gradient(
+            -5deg,
+            transparentize($primary-dark, 0.1),
+            transparentize($primary, 0.1),
+        );
+        backdrop-filter: blur(2px);
+        color: var(--wjb-light);
+        text-shadow: 1px 1px rgba(0, 0, 0, 0.6);
+        box-shadow: 0 2px 12px rgba(0, 0, 0, 0.1), 0 6px 16px -12px rgba(0, 0, 0, 1);
+        border: 1px solid var(--wjb-primary-dark);
+        border-radius: var(--wjb-border-radius);
+
+        h3 {
+            line-height: 1em;
+            margin: 1rem;
+        }
     }
 }
 </style>
