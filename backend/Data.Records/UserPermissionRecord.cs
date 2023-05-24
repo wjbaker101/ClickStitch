75fc8ed9e85dbe93f9fa36e5347e7742c0c0b028
wjbaker101 <willbaker10198@gmail.com>
2023-05-24 21:37:47 +0100

    Add required table field

--- backend/Data.Records/UserPermissionRecord.cs
+++ backend/Data.Records/UserPermissionRecord.cs
@@ -5,6 +5,7 @@ public class UserPermissionRecord : IDatabaseRecord
     public virtual long Id { get; init; }
     public virtual required UserRecord User { get; init; }
     public virtual required PermissionRecord Permission { get; init; }
+    public virtual required DateTime CreatedAt { get; init; }
 }
 
 public sealed class UserPermissionRecordMap : ClassMap<UserPermissionRecord>
@@ -16,5 +17,6 @@ public sealed class UserPermissionRecordMap : ClassMap<UserPermissionRecord>
         Id(x => x.Id, "id").GeneratedBy.SequenceIdentity("user_permission_id_seq");
         References(x => x.User, "user_id");
         References(x => x.Permission, "permission_id");
+        Map(x => x.CreatedAt, "created_at");
     }
 }
