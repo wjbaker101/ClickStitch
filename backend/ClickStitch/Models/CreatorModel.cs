b08fc589734cd221e2e188e3305dd2dc728b8848
wjbaker101 <willbaker10198@gmail.com>
2023-05-24 19:30:46 +0100

    Add model and mapper for creator

---
+++ backend/ClickStitch/Models/CreatorModel.cs
@@ -0,0 +1,9 @@
+﻿namespace ClickStitch.Models;
+
+public sealed class CreatorModel
+{
+    public required Guid Reference { get; init; }
+    public required DateTime CreatedAt { get; init; }
+    public required string Name { get; init; }
+    public required string StoreUrl { get; init; }
+}
---
+++ backend/ClickStitch/Models/Mappers/CreatorMapper.cs
@@ -0,0 +1,14 @@
+﻿using Data.Records;
+
+namespace ClickStitch.Models.Mappers;
+
+public static class CreatorMapper
+{
+    public static CreatorModel Map(CreatorRecord creator) => new()
+    {
+        Reference = creator.Reference,
+        CreatedAt = creator.CreatedAt,
+        Name = creator.Name,
+        StoreUrl = creator.StoreUrl
+    };
+}
