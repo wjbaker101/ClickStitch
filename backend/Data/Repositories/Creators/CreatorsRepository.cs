ef85e8ac22e24f4809d4d91951c420e53da52c5b
wjbaker101 <willbaker10198@gmail.com>
2023-05-24 19:24:21 +0100

    Add repo for creators

---
+++ backend/Data/Repositories/Creators/CreatorsRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace Data.Repositories.Creators;
+
+public interface ICreatorsRepository : IRepository<CreatorRecord>
+{
+}
+
+public sealed class CreatorsRepository : Repository<CreatorRecord>, ICreatorsRepository
+{
+    public CreatorsRepository(IDatabase database) : base(database)
+    {
+    }
+}
