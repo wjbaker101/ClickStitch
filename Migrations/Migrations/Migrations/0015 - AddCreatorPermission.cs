225a588e7e883a0848195ac28b03ea391664346d
wjbaker101 <willbaker10198@gmail.com>
2023-05-24 21:34:33 +0100

    Create migration for new permission

---
+++ Migrations/Migrations/Migrations/0015 - AddCreatorPermission.cs
@@ -0,0 +1,26 @@
+﻿using FluentMigrator;
+using Migrations.Common;
+using Migrations.Records;
+
+namespace Migrations.Migrations;
+
+// ReSharper disable once InconsistentNaming
+[Migration(0015)]
+public sealed class AddCreatorPermission_0015 : Migration
+{
+    public override void Up()
+    {
+        Insert
+            .IntoTable(Names.Tables.PERMISSION)
+            .InSchema(Names.Schemas.CLICK_STITCH)
+            .Row(PermissionRecord.Create(2, "Creator"));
+    }
+
+    public override void Down()
+    {
+        Delete
+            .FromTable(Names.Tables.PERMISSION)
+            .InSchema(Names.Schemas.CLICK_STITCH)
+            .Row(PermissionRecord.Create(2, "Creator"));
+    }
+}
