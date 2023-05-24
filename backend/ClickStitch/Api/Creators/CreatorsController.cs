15c7157d6f181c1642cb82be4797f0e1870a6867
wjbaker101 <willbaker10198@gmail.com>
2023-05-24 19:31:01 +0100

    Create empty Creators endpoint

---
+++ backend/ClickStitch/Api/Creators/CreatorsController.cs
@@ -0,0 +1,15 @@
+﻿using ClickStitch.Types;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ClickStitch.Api.Creators;
+
+[Route("api/creators")]
+public sealed class CreatorsController : ApiController
+{
+    private readonly ICreatorsService _creatorsService;
+
+    public CreatorsController(ICreatorsService creatorsService)
+    {
+        _creatorsService = creatorsService;
+    }
+}
---
+++ backend/ClickStitch/Api/Creators/CreatorsService.cs
@@ -0,0 +1,17 @@
+﻿using Data.Repositories.Creator;
+
+namespace ClickStitch.Api.Creators;
+
+public interface ICreatorsService
+{
+}
+
+public sealed class CreatorsService : ICreatorsService
+{
+    private readonly ICreatorRepository _creatorRepository;
+
+    public CreatorsService(ICreatorRepository creatorRepository)
+    {
+        _creatorRepository = creatorRepository;
+    }
+}
