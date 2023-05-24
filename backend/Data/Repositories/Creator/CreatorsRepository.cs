31e02303f3016a23bd823b2deb5198370649752a
wjbaker101 <willbaker10198@gmail.com>
2023-05-24 19:28:49 +0100

    Rename repo and add to Progam

---
+++ backend/Data/Repositories/Creator/CreatorsRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace Data.Repositories.Creator;
+
+public interface ICreatorRepository : IRepository<CreatorRecord>
+{
+}
+
+public sealed class CreatorRepository : Repository<CreatorRecord>, ICreatorRepository
+{
+    public CreatorRepository(IDatabase database) : base(database)
+    {
+    }
+}
