e8cd1f3b2ed192c0fb0502c3dfd855dae9fd884c
wjbaker101 <willbaker10198@gmail.com>
2023-05-29 09:24:24 +0100

    Create test for creating a creator

---
+++ backend/Tests/Api.Tests/Api/Creators/GivenACreateCreatorRequest.cs
@@ -0,0 +1,46 @@
+﻿using ClickStitch.Api.Creators;
+using ClickStitch.Api.Creators.Types;
+using Core.Types;
+using TestHelpers.Fakes;
+using TestHelpers.Models;
+
+namespace Api.Tests.Api.Creators;
+
+[TestFixture]
+[Parallelizable]
+public sealed class GivenACreateCreatorRequest
+{
+    private Result<CreateCreatorResponse> _result = null!;
+
+    [OneTimeSetUp]
+    public async Task Setup()
+    {
+        var subject = new CreatorsService(FakeCreatorRepository.Default(), null!, null!, null!, null!);
+
+        _result = await subject.CreateCreator(new TestRequestUser(), new CreateCreatorRequest
+        {
+            Name = "TestName",
+            StoreUrl = "TestStoreUrl"
+        }, CancellationToken.None);
+    }
+
+    [Test]
+    public void ThenTheResultIsASuccess()
+    {
+        Assert.That(_result.IsSuccess, Is.True);
+    }
+
+    [Test]
+    public void ThenTheCorrectCreatorIsReturned()
+    {
+        var creator = _result.Content.Creator;
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(creator.Reference, Is.Not.EqualTo(default(Guid)), nameof(creator.Reference));
+            Assert.That(creator.CreatedAt, Is.Not.EqualTo(default(DateTime)), nameof(creator.CreatedAt));
+            Assert.That(creator.Name, Is.EqualTo("TestName"), nameof(creator.Name));
+            Assert.That(creator.StoreUrl, Is.EqualTo("TestStoreUrl"), nameof(creator.StoreUrl));
+        });
+    }
+}
