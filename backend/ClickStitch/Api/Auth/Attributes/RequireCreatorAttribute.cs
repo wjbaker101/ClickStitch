5f2c9a52c9a47f9241aee76332110a161125a3c0
wjbaker101 <willbaker10198@gmail.com>
2023-06-14 23:54:36 +0100

    Add attribute for enforcing creator endpoints

---
+++ backend/ClickStitch/Api/Auth/Attributes/RequireCreatorAttribute.cs
@@ -0,0 +1,22 @@
+﻿using ClickStitch.Helper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace ClickStitch.Api.Auth.Attributes;
+
+[AttributeUsage(AttributeTargets.Method)]
+public sealed class RequireCreatorAttribute : Attribute, IAsyncAuthorizationFilter, IOrderedFilter
+{
+    public int Order => 101;
+
+    public async Task OnAuthorizationAsync(AuthorizationFilterContext context)
+    {
+        var user = RequestHelper.GetRequiredUser(context.HttpContext.Request);
+
+        if (user.Permissions.All(x => x != RequestPermissionType.Creator))
+        {
+            context.Result = new UnauthorizedResult();
+            return;
+        }
+    }
+}
