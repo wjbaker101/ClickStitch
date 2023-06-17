3221d5731684433f8f81d41e478426f2e7fbff26
wjbaker101 <willbaker10198@gmail.com>
2023-06-17 12:29:33 +0100

    Retrieve patterns

--- frontend/src/views/creator/patterns/CreatorPatterns.view.vue
+++ frontend/src/views/creator/patterns/CreatorPatterns.view.vue
@@ -10,11 +10,45 @@
                     <p>Manage your patterns here.</p>
                 </CardComponent>
             </section>
+            <section>
+                <CardComponent border="top" padded>
+                    <ListItemComponent>
+                        <h3>asd</h3>
+                        <template #expanded>
+                            asd123
+                        </template>
+                    </ListItemComponent>
+                </CardComponent>
+            </section>
         </div>
     </ViewComponent>
 </template>
 
 <script setup lang="ts">
+import { onMounted, ref } from 'vue';
+
+import ListItemComponent from '@/components/ListItem.component.vue';
+
+import { api } from '@/api/api';
+import { IGetCreatorPatterns } from '@/models/GetCreatorPatterns.model';
+
+const getPatterns = ref<IGetCreatorPatterns | null>(null);
+
+onMounted(async () => {
+    const selfResult = await api.creators.getSelf();
+    if (selfResult instanceof Error)
+        return;
+
+    if (selfResult === null) {
+        return;
+    }
+
+    const getPatternsResult = await api.creators.getPatterns(selfResult.reference, 5, 1);
+    if (getPatternsResult instanceof Error)
+        return;
+
+    getPatterns.value = getPatternsResult;
+});
 </script>
 
 <style lang="scss">
