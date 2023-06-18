41c0077ca9d037cafcdfa203b74d083ed9534068
wjbaker101 <willbaker10198@gmail.com>
2023-06-18 12:52:15 +0100

    Add logic for updating a pattern

--- frontend/src/views/creator/patterns/components/CreatorPattern.component.vue
+++ frontend/src/views/creator/patterns/components/CreatorPattern.component.vue
@@ -13,8 +13,11 @@
                             <small>{{ pattern.reference }}</small>
                         </div>
                     </div>
+                    <FormInputComponent label="Title">
+                        <input type="text" placeholder="Amazing Pattern" v-model="form.title">
+                    </FormInputComponent>
                     <FormInputComponent label="Shop URL">
-                        <input type="text" placeholder="https://etsy.com/shop/beautifulpatternsco/amazing_pattern" :value="pattern.externalShopUrl">
+                        <input type="text" placeholder="https://etsy.com/shop/beautifulpatternsco/amazing_pattern" v-model="form.externalShopUrl">
                     </FormInputComponent>
                 </FormSectionComponent>
                 <FormSectionComponent>
@@ -29,20 +32,45 @@
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue';
+
 import ListItemComponent from '@/components/ListItem.component.vue';
 import PatternImageComponent from '@/components/shared/PatternImage.component.vue';
 
+import { api } from '@/api/api';
 import { usePopup } from '@wjb/vue/use/popup.use';
 
 import { IPattern } from '@/models/Pattern.model';
 
-defineProps<{
+const props = defineProps<{
     pattern: IPattern;
 }>();
 
+interface IForm {
+    title: string;
+    externalShopUrl: string;
+}
+
 const popup = usePopup();
 
+const form = ref<IForm>({
+    title: props.pattern.title,
+    externalShopUrl: props.pattern.externalShopUrl ?? '',
+});
+
 const onUpdate = async function (): Promise<void> {
+    const result = await api.patterns.update(props.pattern.reference, {
+        title: form.value.title,
+        externalShopUrl: form.value.externalShopUrl,
+    });
+    if (result instanceof Error) {
+        popup.trigger({
+            message: result.message,
+            style: 'error',
+        });
+        return;
+    }
+
     popup.trigger({
         message: 'Pattern updated!',
         style: 'success',
