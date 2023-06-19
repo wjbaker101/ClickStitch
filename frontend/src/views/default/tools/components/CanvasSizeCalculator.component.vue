b9a242ff363cb4a92749b12fde86b97861ba92a3
wjbaker101 <willbaker10198@gmail.com>
2023-06-19 20:51:27 +0100

    Split into component

---
+++ frontend/src/views/default/tools/components/CanvasSizeCalculator.component.vue
@@ -0,0 +1,82 @@
+<template>
+    <h2>Canvas Size Calculator</h2>
+    <div class="canvas-size-calculator-component flex align-items-center gap">
+        <FormComponent>
+            <FormSectionComponent>
+                <FormInputComponent label="Width (Stitches)">
+                    <input type="number" min="1" max="300" v-model="width">
+                </FormInputComponent>
+                <FormInputComponent label="Height (Stitches)">
+                    <input type="number" min="1" max="300" v-model="height">
+                </FormInputComponent>
+                <FormInputComponent label="Aida Stitch Count">
+                    <input type="number" v-model="stitchCount">
+                </FormInputComponent>
+            </FormSectionComponent>
+        </FormComponent>
+        <div class="flex-auto">
+            <IconComponent icon="arrow-right" />
+        </div>
+        <FormComponent>
+            <FormSectionComponent>
+                <p>
+                    <strong>Width</strong>
+                    <br>
+                    {{ (resultWidth * cmToInch).toFixed(1) }}cm / {{ resultWidth.toFixed(1) }}in
+                </p>
+                <p>
+                    <strong>Height</strong>
+                    <br>
+                    {{ (resultHeight * cmToInch).toFixed(1) }}cm / {{ resultHeight.toFixed(1) }}in
+                </p>
+            </FormSectionComponent>
+        </FormComponent>
+        <div>
+            <div>
+                <div class="example" :style="style"></div>
+            </div>
+        </div>
+    </div>
+    <p class="text-centered">Make sure to add a few cm/inches as extra</p>
+</template>
+
+<script setup lang="ts">
+import { computed, ref } from 'vue';
+
+const cmToInch = 2.54;
+const previewSize = 200;
+
+const width = ref<number>(50);
+const height = ref<number>(50);
+const stitchCount = ref<number>(16);
+
+const resultWidth = computed<number>(() => width.value / stitchCount.value);
+const resultHeight = computed<number>(() => height.value / stitchCount.value);
+
+const ratio = computed<number>(() => width.value / height.value);
+
+const style = computed<Record<string, string>>(() => {
+    if (width.value >= height.value) {
+        return {
+            width: `${previewSize}px`,
+            height: `${previewSize * height.value / width.value}px`,
+        };
+    }
+
+    return {
+        height: `${previewSize}px`,
+        width: `${previewSize * ratio.value}px`,
+    };
+});
+</script>
+
+<style lang="scss">
+.canvas-size-calculator-component {
+    .example {
+        margin: auto;
+        border: 1px solid var(--wjb-secondary);
+        border-radius: var(--wjb-border-radius);
+        background-color: var(--wjb-background-colour-light);
+    }
+}
+</style>
