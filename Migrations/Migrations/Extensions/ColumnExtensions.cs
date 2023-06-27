6543fabb35e0a1318f14a8985475bf9eddad1ce6
wjbaker101 <willbaker10198@gmail.com>
2023-06-27 22:52:03 +0100

    Create extensions for creating repeated setups

---
+++ Migrations/Migrations/Extensions/ColumnExtensions.cs
@@ -0,0 +1,16 @@
+﻿using FluentMigrator.Builders.Create.Table;
+
+namespace Migrations.Extensions;
+
+public static class ColumnExtensions
+{
+    public static ICreateTableColumnOptionOrWithColumnSyntax WithIdColumn(this ICreateTableWithColumnSyntax builder)
+    {
+        return builder.WithColumn("id").AsInt64().NotNullable().PrimaryKey().Identity();
+    }
+
+    public static ICreateTableColumnOptionOrWithColumnSyntax AsTimestampWithTimeZone(this ICreateTableColumnAsTypeSyntax column)
+    {
+        return column.AsDateTimeOffset();
+    }
+}
