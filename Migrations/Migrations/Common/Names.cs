bd1d22deaf2b5cb5014b6977d03f1e09ab26dfb3
wjbaker101 <willbaker10198@gmail.com>
2023-06-28 17:34:14 +0100

    Create table for the user_pattern_thread_stitch table

--- Migrations/Migrations/Common/Names.cs
+++ Migrations/Migrations/Common/Names.cs
@@ -18,6 +18,7 @@ public static class Names
         public const string USER_BASKET_ITEM = "user_basket_item";
         public const string USER_PATTERN = "user_pattern";
         public const string USER_PATTERN_STITCH = "user_pattern_stitch";
+        public const string USER_PATTERN_THREAD_STITCH = "user_pattern_thread_stitch";
         public const string USER_PERMISSION = "user_permission";
         public const string CREATOR = "creator";
         public const string USER_CREATOR = "user_creator";
---
+++ Migrations/Migrations/Migrations/0019 - CreateUserPatternThreadStitchTable.cs
@@ -0,0 +1,55 @@
+﻿using FluentMigrator;
+using Migrations.Common;
+using Migrations.Extensions;
+
+namespace Migrations.Migrations;
+
+// ReSharper disable once InconsistentNaming
+[Migration(0019)]
+public sealed class CreateUserPatternThreadStitchTable_0019 : Migration
+{
+    public override void Up()
+    {
+        Create
+            .Table(Names.Tables.USER_PATTERN_THREAD_STITCH)
+            .InSchema(Names.Schemas.CLICK_STITCH)
+            .WithIdColumn()
+            .WithColumn("user_id").AsInt64().NotNullable()
+            .WithColumn("pattern_thread_stitch_id").AsInt64().NotNullable()
+            .WithColumn("stitched_at").AsTimestampWithTimeZone().NotNullable();
+
+        Create
+            .ForeignKey()
+            .FromTable(Names.Tables.USER_PATTERN_THREAD_STITCH)
+            .InSchema(Names.Schemas.CLICK_STITCH)
+            .ForeignColumn("user_id")
+            .ToTable(Names.Tables.USER)
+            .InSchema(Names.Schemas.CLICK_STITCH)
+            .PrimaryColumn("id");
+
+        Create
+            .ForeignKey()
+            .FromTable(Names.Tables.USER_PATTERN_THREAD_STITCH)
+            .InSchema(Names.Schemas.CLICK_STITCH)
+            .ForeignColumn("pattern_thread_stitch_id")
+            .ToTable(Names.Tables.PATTERN_THREAD_STITCH)
+            .InSchema(Names.Schemas.CLICK_STITCH)
+            .PrimaryColumn("id");
+
+        Create
+            .Index()
+            .OnTable(Names.Tables.USER_PATTERN_THREAD_STITCH)
+            .InSchema(Names.Schemas.CLICK_STITCH)
+            .OnColumn("user_id").Ascending()
+            .OnColumn("pattern_thread_stitch_id").Ascending()
+            .WithOptions()
+            .Unique();
+    }
+
+    public override void Down()
+    {
+        Delete
+            .Table(Names.Tables.USER_PATTERN_THREAD_STITCH)
+            .InSchema(Names.Schemas.CLICK_STITCH);
+    }
+}
