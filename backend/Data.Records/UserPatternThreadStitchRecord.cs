0d47419da2df7ef1c93437c9606ddba4cb5a3079
wjbaker101 <willbaker10198@gmail.com>
2023-06-28 17:37:12 +0100

    Create record/repo for new table

---
+++ backend/Data.Records/UserPatternThreadStitchRecord.cs
@@ -0,0 +1,22 @@
+﻿namespace Data.Records;
+
+public class UserPatternThreadStitchRecord : IDatabaseRecord
+{
+    public virtual long Id { get; init; }
+    public virtual required UserRecord User { get; init; }
+    public virtual required PatternThreadStitchRecord Stitch { get; init; }
+    public virtual required DateTime StitchedAt { get; set; }
+}
+
+public sealed class UserPatternThreadStitchRecordMap : ClassMap<UserPatternThreadStitchRecord>
+{
+    public UserPatternThreadStitchRecordMap()
+    {
+        Schema(DatabaseValues.SCHEMA);
+        Table("user_pattern_thread_stitch");
+        Id(x => x.Id, "id").GeneratedBy.SequenceIdentity("user_pattern_thread_stitch_id_seq");
+        References(x => x.User, "user_id");
+        References(x => x.Stitch, "pattern_thread_stitch_id");
+        Map(x => x.StitchedAt, "stitched_at");
+    }
+}
---
+++ backend/Data/Repositories/UserPatternThreadStitch/UserPatternStitchRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace Data.Repositories.UserPatternThreadStitch;
+
+public interface IUserPatternThreadStitchRepository : IRepository<UserPatternThreadStitchRecord>
+{
+}
+
+public sealed class UserPatternThreadStitchRepository : Repository<UserPatternThreadStitchRecord>, IUserPatternThreadStitchRepository
+{
+    public UserPatternThreadStitchRepository(IDatabase database) : base(database)
+    {
+    }
+}
