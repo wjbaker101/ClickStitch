6b02f6f16b10a4fffc60b29a84717af1ba33d228
wjbaker101 <willbaker10198@gmail.com>
2023-07-01 10:06:13 +0100

    Delete unused code

--- backend/Data/Repositories/UserPatternStitch/UserPatternStitchRepository.cs
+++ backend/Data/Repositories/UserPatternStitch/UserPatternStitchRepository.cs
@@ -2,36 +2,13 @@
 
 public interface IUserPatternStitchRepository : IRepository<UserPatternStitchRecord>
 {
-    Task<Result> DeleteByPositions(UserPatternRecord userPattern, List<(int posX, int posY)> positions);
     Task<Dictionary<long, UserPatternStitchRecord>> GetByUserPattern(UserPatternRecord userPattern, CancellationToken cancellationToken);
-    Task<Result> CompleteByPositions(PatternRecord pattern, UserPatternRecord userPattern, List<(int posX, int posY)> positions);
 }
 
 public sealed class UserPatternStitchRepository : Repository<UserPatternStitchRecord>, IUserPatternStitchRepository
 {
     public UserPatternStitchRepository(IDatabase database) : base(database)
     {
-    }
-
-    public async Task<Result> DeleteByPositions(UserPatternRecord userPattern, List<(int posX, int posY)> positions)
-    {
-        using var session = Database.SessionFactory.OpenSession();
-        using var transaction = session.BeginTransaction();
-
-        foreach (var position in positions)
-        {
-            await session
-                .Query<UserPatternStitchRecord>()
-                .Where(x =>
-                    x.UserPattern == userPattern &&
-                    x.X == position.posX &&
-                    x.Y == position.posY)
-                .DeleteAsync();
-        }
-
-        await transaction.CommitAsync();
-
-        return Result.Success();
     }
 
     public async Task<Dictionary<long, UserPatternStitchRecord>> GetByUserPattern(UserPatternRecord userPattern, CancellationToken cancellationToken)
@@ -44,37 +21,8 @@ public sealed class UserPatternStitchRepository : Repository<UserPatternStitchRe
             .Where(x => x.UserPattern == userPattern)
             .ToDictionary(x => x.Stitch.Id, x => x);
 
-        await transaction.CommitAsync();
+        await transaction.CommitAsync(cancellationToken);
 
         return userPatterns;
-    }
-
-    public async Task<Result> CompleteByPositions(PatternRecord pattern, UserPatternRecord userPattern, List<(int posX, int posY)> positions)
-    {
-        using var session = Database.SessionFactory.OpenSession();
-        using var transaction = session.BeginTransaction();
-
-        foreach (var position in positions)
-        {
-            var stitch = await session
-                .Query<PatternStitchRecord>()
-                .SingleOrDefaultAsync(x =>
-                    x.Pattern == pattern &&
-                    x.X == position.posX &&
-                    x.Y == position.posY);
-
-            await session.SaveAsync(new UserPatternStitchRecord
-            {
-                UserPattern = userPattern,
-                Stitch = stitch,
-                StitchedAt = DateTime.UtcNow,
-                X = stitch.X,
-                Y = stitch.Y
-            });
-        }
-
-        await transaction.CommitAsync();
-
-        return Result.Success();
     }
 }
