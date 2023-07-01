3cc185bb718474afd545ee5841ca59b96d7437e0
wjbaker101 <willbaker10198@gmail.com>
2023-07-02 00:43:52 +0100

    Make lookup hash non-nullable

---
+++ Migrations/Migrations/Migrations/0023 - SetLookupHashColumnAsNonNullable.cs
@@ -0,0 +1,29 @@
+﻿using FluentMigrator;
+using Migrations.Common;
+
+namespace Migrations.Migrations;
+
+// ReSharper disable once InconsistentNaming
+[Migration(0023)]
+public sealed class SetLookupHashColumnAsNonNullable_0023 : Migration
+{
+    public override void Up()
+    {
+        Alter
+            .Column("lookup_hash")
+            .OnTable(Names.Tables.PATTERN_THREAD_STITCH)
+            .InSchema(Names.Schemas.CLICK_STITCH)
+            .AsString()
+            .NotNullable();
+    }
+
+    public override void Down()
+    {
+        Alter
+            .Column("lookup_hash")
+            .OnTable(Names.Tables.PATTERN_THREAD_STITCH)
+            .InSchema(Names.Schemas.CLICK_STITCH)
+            .AsString()
+            .Nullable();
+    }
+}
