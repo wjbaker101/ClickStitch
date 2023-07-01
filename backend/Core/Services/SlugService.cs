aeb973429ded6cca6910c3fdd9a428a33da4a455
wjbaker101 <willbaker10198@gmail.com>
2023-07-01 21:58:24 +0100

    Create a service for generating slugs from a string

---
+++ backend/Core/Services/SlugService.cs
@@ -0,0 +1,27 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Core.Services;
+
+public static partial class SlugService
+{
+    [GeneratedRegex(@"[^a-z0-9-\s]")]
+    private static partial Regex AlphanumericRegex();
+
+    [GeneratedRegex("\\s+")]
+    private static partial Regex TrimSpacesRegex();
+
+    [GeneratedRegex("-+")]
+    private static partial Regex TrimDashesRegex();
+
+    public static string Generate(string text)
+    {
+        var mutated = text;
+
+        mutated = AlphanumericRegex().Replace(mutated.ToLower().Trim(), "");
+        mutated = TrimSpacesRegex().Replace(mutated, " ");
+        mutated = TrimDashesRegex().Replace(mutated, " ");
+        mutated = mutated.Replace(" ", "-");
+
+        return mutated;
+    }
+}
---
+++ backend/Tests/Core.Tests/Services/SlugServiceTests/GivenAGenerateRequest.cs
@@ -0,0 +1,35 @@
+﻿using Core.Services;
+
+namespace Core.Tests.Services.SlugServiceTests;
+
+[TestFixture("Test Pattern TITLE", "test-pattern-title")]
+[TestFixture("Test Pattern Title!!!", "test-pattern-title")]
+[TestFixture("test_pattern_title", "testpatterntitle")]
+[TestFixture("test----pattern----title", "test-pattern-title")]
+[TestFixture("!!test--123!----title!", "test-123-title")]
+[Parallelizable]
+public sealed class GivenAGenerateRequest
+{
+    private readonly string _incomingText;
+    private readonly string _expectedSlug;
+
+    private string _result = null!;
+
+    public GivenAGenerateRequest(string incomingText, string expectedSlug)
+    {
+        _incomingText = incomingText;
+        _expectedSlug = expectedSlug;
+    }
+
+    [OneTimeSetUp]
+    public void Setup()
+    {
+        _result = SlugService.Generate(_incomingText);
+    }
+
+    [Test]
+    public void ThenTheCorrectResultIsReturned()
+    {
+        Assert.That(_result, Is.EqualTo(_expectedSlug));
+    }
+}
