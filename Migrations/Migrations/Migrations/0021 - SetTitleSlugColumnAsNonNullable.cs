4642d79de7f8a866b95e0452c83115185e96c270
wjbaker101 <willbaker10198@gmail.com>
2023-07-01 22:46:31 +0100

    Make titl;e slug column non-nullable

---
+++ Migrations/Migrations/Migrations/0021 - SetTitleSlugColumnAsNonNullable.cs
@@ -0,0 +1,29 @@
+﻿using FluentMigrator;
+using Migrations.Common;
+
+namespace Migrations.Migrations;
+
+// ReSharper disable once InconsistentNaming
+[Migration(0021)]
+public sealed class SetTitleSlugColumnAsNonNullable_0021 : Migration
+{
+    public override void Up()
+    {
+        Alter
+            .Column("title_slug")
+            .OnTable(Names.Tables.PATTERN)
+            .InSchema(Names.Schemas.CLICK_STITCH)
+            .AsString()
+            .NotNullable();
+    }
+
+    public override void Down()
+    {
+        Alter
+            .Column("title_slug")
+            .OnTable(Names.Tables.PATTERN)
+            .InSchema(Names.Schemas.CLICK_STITCH)
+            .AsString()
+            .Nullable();
+    }
+}
