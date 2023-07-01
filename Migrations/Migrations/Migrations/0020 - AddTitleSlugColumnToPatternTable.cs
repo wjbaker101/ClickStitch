7eee286a29409c22c55b29ff3d560f3ad776ccf4
wjbaker101 <willbaker10198@gmail.com>
2023-07-01 22:11:28 +0100

    Add title slug column

---
+++ Migrations/Migrations/Migrations/0020 - AddTitleSlugColumnToPatternTable.cs
@@ -0,0 +1,36 @@
+﻿using FluentMigrator;
+using Migrations.Common;
+
+namespace Migrations.Migrations;
+
+// ReSharper disable once InconsistentNaming
+[Migration(0020)]
+public sealed class AddTitleSlugColumnToPatternTable_0020 : Migration
+{
+    public override void Up()
+    {
+        Create
+            .Column("title_slug")
+            .OnTable(Names.Tables.PATTERN)
+            .InSchema(Names.Schemas.CLICK_STITCH)
+            .AsString()
+            .Nullable();
+
+        Create
+            .Index()
+            .OnTable(Names.Tables.PATTERN)
+            .InSchema(Names.Schemas.CLICK_STITCH)
+            .OnColumn("title_slug")
+            .Ascending()
+            .WithOptions()
+            .Unique();
+    }
+
+    public override void Down()
+    {
+        Delete
+            .Column("title_slug")
+            .FromTable(Names.Tables.PATTERN)
+            .InSchema(Names.Schemas.CLICK_STITCH);
+    }
+}
