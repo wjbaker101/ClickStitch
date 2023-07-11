2b5bac72fe4ec2d776de9665be4c8d717b134f30
wjbaker101 <willbaker10198@gmail.com>
2023-07-11 19:50:00 +0100

    Fix verify api client

--- frontend/src/api/api.ts
+++ frontend/src/api/api.ts
@@ -376,7 +376,7 @@ export const api = {
             }
         },
 
-        async verify(patternReference: string, patternData: string): Promise<boolean | Error> {
+        async verify(patternData: string): Promise<boolean | Error> {
             if (auth.details.value === null)
                 return new Error('You must be logged in for this action.');
 
@@ -384,7 +384,7 @@ export const api = {
                 const formData = new FormData();
                 formData.append('pattern_data', patternData);
 
-                const response = await client.post<IApiResultResponse<{}>>(`/patterns/${patternReference}`, formData, {
+                const response = await client.post<IApiResultResponse<{}>>('/patterns/verify', formData, {
                     headers: {
                         'Authorization': `Bearer ${auth.details.value.loginToken}`,
                     },
