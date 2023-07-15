22a629c9d3dd6f7d184d2155959fca93fd576f02
wjbaker101 <willbaker10198@gmail.com>
2023-07-15 02:03:48 +0100

    Migrate from vue-cli to vite

---
+++ frontend/vite.config.js
@@ -0,0 +1,37 @@
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import mkcert from 'vite-plugin-mkcert';
+import path from 'path';
+
+export default defineConfig({
+
+    plugins: [
+        vue(),
+        mkcert(),
+    ],
+
+    resolve: {
+        alias: [
+            {
+                find: /^~(.*)$/,
+                replacement: '$1',
+            },
+            {
+                find: '@',
+                replacement: path.resolve(__dirname,'src'),
+            }
+        ],
+    },
+
+    server: {
+        https: true,
+        proxy: {
+            '/api': {
+                target: 'https://localhost:44371',
+                changeOrigin: true,
+                secure: false,
+            },
+        },
+    },
+
+});
