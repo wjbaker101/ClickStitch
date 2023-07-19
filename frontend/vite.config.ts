2916efddc72d976d3486213eed08e9fee7705a69
William Baker <wjbaker101@users.noreply.github.com>
2023-07-19 21:19:07 +0100

    Add pwa

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import mkcert from 'vite-plugin-mkcert';
+import { VitePWA as pwa } from 'vite-plugin-pwa'
 import path from 'path';
 
 export default defineConfig({
@@ -8,6 +9,12 @@ export default defineConfig({
     plugins: [
         vue(),
         mkcert(),
+        pwa({
+            registerType: 'autoUpdate',
+            devOptions: {
+                enabled: true,
+            },
+        }),
     ],
 
     resolve: {
