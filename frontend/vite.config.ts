67b07bdf920072a5d68e4b99830db612394696aa
William Baker <wjbaker101@users.noreply.github.com>
2023-07-19 22:05:49 +0100

    Add pwa images

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -11,6 +11,25 @@ export default defineConfig({
         mkcert(),
         pwa({
             registerType: 'autoUpdate',
+            includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+            manifest: {
+                name: 'ClickStitch',
+                short_name: 'ClickStitch',
+                description: 'A companion for tracking your cross-stitching progress!',
+                theme_color: '#22927f',
+                icons: [
+                    {
+                        src: 'pwa-192x192.png',
+                        sizes: '192x192',
+                        type: 'image/png',
+                    },
+                    {
+                        src: 'pwa-512x512.png',
+                        sizes: '512x512',
+                        type: 'image/png',
+                    },
+                ],
+            },
             devOptions: {
                 enabled: true,
             },
