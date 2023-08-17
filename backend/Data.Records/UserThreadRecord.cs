500ea95287ad2ff27c7e5a3c22500a8f1d90b553
William Baker <wjbaker101@users.noreply.github.com>
2023-08-17 23:13:18 +0100

    Add count

--- backend/Data.Records/UserThreadRecord.cs
+++ backend/Data.Records/UserThreadRecord.cs
@@ -5,6 +5,7 @@ public class UserThreadRecord : IDatabaseRecord
     public virtual required long Id { get; init; }
     public virtual required UserRecord User { get; init; }
     public virtual required ThreadRecord Thread { get; init; }
+    public virtual required int Count { get; init; }
 }
 
 public sealed class UserThreadRecordMap : ClassMap<UserThreadRecord>
@@ -16,5 +17,6 @@ public sealed class UserThreadRecordMap : ClassMap<UserThreadRecord>
         Id(x => x.Id, "id").GeneratedBy.SequenceIdentity("user_thread_id_seq");
         References(x => x.User, "user_id");
         References(x => x.Thread, "thread_id");
+        Map(x => x.Count, "count");
     }
 }
