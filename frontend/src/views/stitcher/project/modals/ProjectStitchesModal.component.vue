cd2bb73fd87e5759e9a2213d80cc108e70869579
William Baker <wjbaker101@users.noreply.github.com>
2023-09-03 11:34:07 +0100

    Move into component

--- frontend/src/views/stitcher/project/modals/ProjectStitchesModal.component.vue
+++ frontend/src/views/stitcher/project/modals/ProjectStitchesModal.component.vue
@@ -2,58 +2,21 @@
     <div class="project-stitches-modal-component">
         <h2>Stitches</h2>
         <div class="stitches">
-            <ListItemComponent v-for="thread in threads">
-                <div class="flex align-items-center">
-                    <div>
-                        <div class="thread-colour text-centered" :style="threadStyle(thread.thread)">
-                            {{ thread.thread.index }}
-                        </div>
-                        <span class="thread-text">
-                            <strong>{{ thread.thread.name }}</strong> - <small>{{ thread.thread.description }}</small>
-                        </span>
-                    </div>
-                    <div class="flex-auto">
-                        {{ thread.completedStitches.length }} <small>/ {{ thread.stitches.length }}</small>
-                    </div>
-                </div>
-                <template #expanded>
-                    <div class="thread-actions flex gap align-items-center">
-                        <div>
-                            <strong>{{ getPercentage(thread) }}%</strong> Completed
-                        </div>
-                    </div>
-                </template>
-            </ListItemComponent>
+            <ThreadDetailsComponent v-for="thread in threads" :thread="thread" />
         </div>
     </div>
 </template>
 
 <script setup lang="ts">
-import type { StyleValue } from 'vue';
+import ThreadDetailsComponent from '../components/ThreadDetails.component.vue';
 
-import ListItemComponent from '@/components/ListItem.component.vue';
-
-import { isDark } from '@/helper/helper';
-
-import type { IGetProject, IThreadDetails } from '@/models/GetProject.model';
-import type { IPatternThread } from '@/models/Pattern.model';
+import type { IGetProject } from '@/models/GetProject.model';
 
 const props = defineProps<{
     project: IGetProject;
 }>();
 
 const threads = props.project.threads;
-
-const threadStyle = function (thread: IPatternThread): StyleValue {
-    return {
-        backgroundColor: thread.colour,
-        color: isDark(thread.colour) ? '#fff' : '#000',
-    };
-};
-
-const getPercentage = function (thread: IThreadDetails): string {
-    return (thread.completedStitches.length / thread.stitches.length * 100).toFixed(1);
-};
 </script>
 
 <style lang="scss">
