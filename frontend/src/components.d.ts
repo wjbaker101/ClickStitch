30c3a7e26d4e2f497648625658ade7f35d3b7a37
William Baker <wjbaker101@users.noreply.github.com>
2023-09-11 19:24:42 +0100

    Add ButtonComponent as a global component

--- frontend/src/components.d.ts
+++ frontend/src/components.d.ts
@@ -1,5 +1,6 @@
 declare module '@vue/runtime-core' {
     export interface GlobalComponents {
+        ButtonComponent: typeof import('@wjb/vue/components/ButtonComponent.vue').default;
         CardComponent: typeof import('@/components/Card.component.vue').default;
         LinkComponent: typeof import('@/components/Link.component.vue').default;
         ViewComponent: typeof import('@/components/View.component.vue').default;
