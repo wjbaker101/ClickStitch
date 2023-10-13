6c6491b56fde51055e4b4b3f2906616827a09821
William Baker <wjbaker101@users.noreply.github.com>
2023-10-13 20:04:21 +0100

    Add migrations for adding threads

---
+++ Migrations/Migrations/Records/ThreadRecord.cs
@@ -0,0 +1,11 @@
+﻿namespace Migrations.Records;
+
+public static class ThreadRecord
+{
+    public static object Create(Guid reference, string brand, string code) => new
+    {
+        reference,
+        brand,
+        code
+    };
+}
