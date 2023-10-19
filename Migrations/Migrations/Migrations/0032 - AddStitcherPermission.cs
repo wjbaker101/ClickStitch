f0ab93ed8a6acc3b3088ae3edda686a1853ef7fd
William Baker <wjbaker101@users.noreply.github.com>
2023-10-19 17:59:22 +0100

    Add new permission

---
+++ Migrations/Migrations/Migrations/0032 - AddStitcherPermission.cs
@@ -0,0 +1,26 @@
+﻿using FluentMigrator;
+using Migrations.Common;
+using Migrations.Records;
+
+namespace Migrations.Migrations;
+
+// ReSharper disable once InconsistentNaming
+[Migration(0032)]
+public sealed class AddStitcherPermission_0032 : Migration
+{
+    public override void Up()
+    {
+        Insert
+            .IntoTable(Names.Tables.PERMISSION)
+            .InSchema(Names.Schemas.CLICK_STITCH)
+            .Row(PermissionRecord.Create(3, "Stitcher"));
+    }
+
+    public override void Down()
+    {
+        Delete
+            .FromTable(Names.Tables.PERMISSION)
+            .InSchema(Names.Schemas.CLICK_STITCH)
+            .Row(PermissionRecord.Create(3, "Stitcher"));
+    }
+}
