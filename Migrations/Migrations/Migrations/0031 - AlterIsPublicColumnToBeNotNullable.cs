237665da95a72a9f6c23227962e4e50f809e9db4
William Baker <wjbaker101@users.noreply.github.com>
2023-10-19 17:13:35 +0100

    Set column to not-nullable, table has been backfilled and code updated to always set the value

---
+++ Migrations/Migrations/Migrations/0031 - AlterIsPublicColumnToBeNotNullable.cs
@@ -0,0 +1,29 @@
+﻿using FluentMigrator;
+using Migrations.Common;
+
+namespace Migrations.Migrations;
+
+// ReSharper disable once InconsistentNaming
+[Migration(0031)]
+public sealed class AlterIsPublicColumnToBeNotNullable_0031 : Migration
+{
+    public override void Up()
+    {
+        Alter
+            .Column("is_public")
+            .OnTable(Names.Tables.PATTERN)
+            .InSchema(Names.Schemas.CLICK_STITCH)
+            .AsBoolean()
+            .NotNullable();
+    }
+
+    public override void Down()
+    {
+        Alter
+            .Column("is_public")
+            .OnTable(Names.Tables.PATTERN)
+            .InSchema(Names.Schemas.CLICK_STITCH)
+            .AsBoolean()
+            .Nullable();
+    }
+}
