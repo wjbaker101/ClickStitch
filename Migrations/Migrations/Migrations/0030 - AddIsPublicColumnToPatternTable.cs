dc40d5227db4a3feb74d26bc1bab3990e4532959
William Baker <wjbaker101@users.noreply.github.com>
2023-10-19 17:10:48 +0100

    Add an is_public column for patterns so that stitcher-created patterns can be seen only by themselves

---
+++ Migrations/Migrations/Migrations/0030 - AddIsPublicColumnToPatternTable.cs
@@ -0,0 +1,27 @@
+﻿using FluentMigrator;
+using Migrations.Common;
+
+namespace Migrations.Migrations;
+
+// ReSharper disable once InconsistentNaming
+[Migration(0030)]
+public sealed class AddIsPublicColumnToPatternTable_0030 : Migration
+{
+    public override void Up()
+    {
+        Alter
+            .Table(Names.Tables.PATTERN)
+            .InSchema(Names.Schemas.CLICK_STITCH)
+            .AddColumn("is_public")
+            .AsBoolean()
+            .Nullable();
+    }
+
+    public override void Down()
+    {
+        Delete
+            .Column("is_public")
+            .FromTable(Names.Tables.PATTERN)
+            .InSchema(Names.Schemas.CLICK_STITCH);
+    }
+}
