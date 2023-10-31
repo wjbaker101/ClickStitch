4231d22a1236bfc9d958741d744451fc813a4a48
William Baker <wjbaker101@users.noreply.github.com>
2023-10-31 21:42:30 +0000

    Remove now unused column

---
+++ Migrations/Migrations/Migrations/0033 - RemoveCreatorIdColumnFromPatternTable.cs
@@ -0,0 +1,21 @@
+﻿using FluentMigrator;
+using Migrations.Common;
+
+namespace Migrations.Migrations;
+
+// ReSharper disable once InconsistentNaming
+[Migration(0033)]
+public sealed class RemoveCreatorIdColumnFromPatternTable_0033 : Migration
+{
+    public override void Up()
+    {
+        Delete
+            .Column("creator_id")
+            .FromTable(Names.Tables.PATTERN)
+            .InSchema(Names.Schemas.CLICK_STITCH);
+    }
+
+    public override void Down()
+    {
+    }
+}
