803e03fd59b9c573eb64806abe8b5efa05fbdd07
William Baker <wjbaker101@users.noreply.github.com>
2023-11-15 20:02:51 +0000

    Rename columns to match name of the action of what they describe

---
+++ Migrations/Migrations/Migrations/0035 - RenamePositionColumnsInUserPatternTable.cs
@@ -0,0 +1,24 @@
+﻿using FluentMigrator;
+using Migrations.Common;
+
+namespace Migrations.Migrations;
+
+// ReSharper disable once InconsistentNaming
+[Migration(0035)]
+public sealed class RenamePositionColumnsInUserPatternTable_0035 : AutoReversingMigration
+{
+    public override void Up()
+    {
+        Rename
+            .Column("last_position_x")
+            .OnTable(Names.Tables.USER_PATTERN)
+            .InSchema(Names.Schemas.CLICK_STITCH)
+            .To("pause_position_x");
+
+        Rename
+            .Column("last_position_y")
+            .OnTable(Names.Tables.USER_PATTERN)
+            .InSchema(Names.Schemas.CLICK_STITCH)
+            .To("pause_position_y");
+    }
+}
