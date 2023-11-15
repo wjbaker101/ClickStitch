188e49846bc80d7d211bb86ce75baa93dfacf887
William Baker <wjbaker101@users.noreply.github.com>
2023-11-15 19:35:05 +0000

    Add columns for saving the latest stitch position

---
+++ Migrations/Migrations/Migrations/0034 - AddPositionColumnsToUserPatternTable.cs
@@ -0,0 +1,20 @@
+﻿using FluentMigrator;
+using Migrations.Common;
+
+namespace Migrations.Migrations;
+
+// ReSharper disable once InconsistentNaming
+[Migration(0034)]
+public sealed class AddPositionColumnsToUserPatternTable_0034 : AutoReversingMigration
+{
+    public override void Up()
+    {
+        Alter
+            .Table(Names.Tables.USER_PATTERN)
+            .InSchema(Names.Schemas.CLICK_STITCH)
+            .AddColumn("last_position_x")
+            .AsInt32().Nullable()
+            .AddColumn("last_position_y")
+            .AsInt32().Nullable();
+    }
+}
