8fd219860698684a099f5919b7aee3680532c40d
William Baker <wjbaker101@users.noreply.github.com>
2023-11-19 23:06:04 +0000

    Move out so composable can be reused

--- frontend/src/views/stitcher/project/use/Mouse.use.ts
+++ frontend/src/views/stitcher/project/use/Mouse.use.ts
@@ -2,16 +2,16 @@ import { ref } from 'vue';
 
 import { Position } from '@/class/Position.class';
 
+const mousePosition = ref<Position>(Position.ZERO);
+const prevMousePosition = ref<Position>(Position.ZERO);
+
+const isDragMoving = ref<boolean>(false);
+const isDragSelecting = ref<boolean>(false);
+
+const selectStart = ref<Position | null>(null);
+const selectEnd = ref<Position | null>(null);
+
 export const useMouse = function () {
-    const mousePosition = ref<Position>(Position.ZERO);
-    const prevMousePosition = ref<Position>(Position.ZERO);
-
-    const isDragMoving = ref<boolean>(false);
-    const isDragSelecting = ref<boolean>(false);
-
-    const selectStart = ref<Position | null>(null);
-    const selectEnd = ref<Position | null>(null);
-
     return {
 
         mousePosition,
