d8725fca7aa3b9885668f142d3bd67c9e34ddd45
William Baker <wjbaker101@users.noreply.github.com>
2023-11-26 21:33:35 +0000

    Readd button for adding patterns to dashboard

--- frontend/src/views/stitcher/patterns/components/Pattern.component.vue
+++ frontend/src/views/stitcher/patterns/components/Pattern.component.vue
@@ -16,16 +16,27 @@
                         <span>View Page</span>
                     </ButtonComponent>
                 </a>
+                <div v-if="authDetails !== null" class="flex">
+                    <ButtonComponent title="Add to Your Dashboard" @click.stop="onAddToBasket(pattern)">
+                        <IconComponent icon="plus" gap="right" />
+                        <span>Add to Dashboard</span>
+                    </ButtonComponent>
+                </div>
             </div>
         </div>
     </CardComponent>
 </template>
 
 <script setup lang="ts">
+import { useRouter } from 'vue-router';
+
 import PatternImageComponent from '@/components/shared/PatternImage.component.vue';
 import PatternImagesModal from '@/views/stitcher/patterns/modal/PatternImagesModal.component.vue';
 
+import { api } from '@/api/api';
+import { useAuth } from '@/use/auth/Auth.use';
 import { useModal } from '@wjb/vue/use/modal.use';
+import { usePopup } from '@wjb/vue/use/popup.use';
 
 import { type IPattern } from '@/models/Pattern.model';
 
@@ -33,7 +44,12 @@ const props = defineProps<{
     pattern: IPattern;
 }>();
 
+const auth = useAuth();
 const modal = useModal();
+const popup = usePopup();
+const router = useRouter();
+
+const authDetails = auth.details;
 
 const onClick = function (): void {
     modal.show({
@@ -42,6 +58,17 @@ const onClick = function (): void {
             pattern: props.pattern,
         },
     });
+};
+
+const onAddToBasket = async function (pattern: IPattern): Promise<void> {
+    await api.basket.quickAdd(pattern.reference);
+
+    popup.trigger({
+        message: `${props.pattern.title} has been added to your dashboard!`,
+        style: 'success',
+    });
+
+    router.push({ path: '/dashboard' });
 };
 </script>
 
