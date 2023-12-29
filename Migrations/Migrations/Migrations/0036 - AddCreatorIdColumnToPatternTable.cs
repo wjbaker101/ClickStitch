60e02a6a06ca7f9ad268ca7c6c398ab3813d453d
William Baker <wjbaker101@users.noreply.github.com>
2023-12-29 21:17:52 +0000

    Add creator id to patterns as an explicit link to the creator without having to optionally join from the userId

---
+++ Migrations/Migrations/Migrations/0036 - AddCreatorIdColumnToPatternTable.cs
@@ -0,0 +1,19 @@
+﻿using FluentMigrator;
+using Migrations.Common;
+
+namespace Migrations.Migrations;
+
+// ReSharper disable once InconsistentNaming
+[Migration(0036)]
+public sealed class AddCreatorIdColumnToPatternTable_0036 : AutoReversingMigration
+{
+    public override void Up()
+    {
+        Create
+            .Column("creator_id")
+            .OnTable(Names.Tables.PATTERN)
+            .InSchema(Names.Schemas.CLICK_STITCH)
+            .AsInt64()
+            .Nullable();
+    }
+}
