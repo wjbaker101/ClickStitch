61725918408f1a6185293be4a79a90c5c4c22438
William Baker <wjbaker101@users.noreply.github.com>
2024-01-01 12:16:11 +0000

    Tests

---
+++ backend/Tests/Api.Tests/Api/Patterns/VerifyPattern/GivenAVerifyPatternRequest.cs
@@ -0,0 +1,27 @@
+﻿using ClickStitch.Api.Patterns;
+using ClickStitch.Api.Patterns.Types;
+
+namespace Api.Tests.Api.Patterns.VerifyPattern;
+
+[TestFixture]
+[Parallelizable]
+public sealed class GivenAVerifyPatternRequest
+{
+    private Result<VerifyPatternResponse> _result = null!;
+
+    [OneTimeSetUp]
+    public async Task Setup()
+    {
+        var patternParserService = new FakePatternParserService();
+
+        var subject = new PatternsService(null!, null!, null!, null!, null!, null!, null!, patternParserService);
+
+        _result = await subject.VerifyPattern(null!, CancellationToken.None);
+    }
+
+    [Test]
+    public void ThenTheResultIsASuccess()
+    {
+        Assert.That(_result.IsSuccess, Is.True);
+    }
+}
---
+++ backend/Tests/Api.Tests/Api/Patterns/VerifyPattern/GivenAVerifyPatternRequestForAnInvalidPattern.cs
@@ -0,0 +1,37 @@
+﻿using ClickStitch.Api.Patterns;
+using ClickStitch.Api.Patterns.Parsing.Types;
+using ClickStitch.Api.Patterns.Types;
+
+namespace Api.Tests.Api.Patterns.VerifyPattern;
+
+[TestFixture]
+[Parallelizable]
+public sealed class GivenAVerifyPatternRequestForAnInvalidPattern
+{
+    private Result<VerifyPatternResponse> _result = null!;
+
+    [OneTimeSetUp]
+    public async Task Setup()
+    {
+        var patternParserService = new FakePatternParserService
+        {
+            Response = Result<ParsePatternResponse>.Failure("TestError")
+        };
+
+        var subject = new PatternsService(null!, null!, null!, null!, null!, null!, null!, patternParserService);
+
+        _result = await subject.VerifyPattern(null!, CancellationToken.None);
+    }
+
+    [Test]
+    public void ThenTheResultIsAFailure()
+    {
+        Assert.That(_result.IsFailure, Is.True);
+    }
+
+    [Test]
+    public void ThenTheCorrectErrorIsReturned()
+    {
+        Assert.That(_result.FailureMessage, Is.EqualTo("TestError"));
+    }
+}
