de312f9db5df7cbb6896998cfa39ba8ffddaaeef
William Baker <wjbaker101@users.noreply.github.com>
2024-01-04 21:10:57 +0000

    Create a test endpoint to make sure logging to Inkwell works

---
+++ backend/ClickStitch/Api/Test/TestController.cs
@@ -0,0 +1,15 @@
+﻿using DotNetLibs.Api.Types;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ClickStitch.Api.Test;
+
+[Route("api/test")]
+public sealed class TestController : ApiController
+{
+    [HttpGet]
+    [Route("")]
+    public IActionResult ThrowException()
+    {
+        throw new Exception("Throwing an exception...");
+    }
+}
