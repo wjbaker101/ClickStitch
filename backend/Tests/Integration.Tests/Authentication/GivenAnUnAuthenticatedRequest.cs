1c59c68903321074aa5fb88dad825a125831bdf3
William Baker <wjbaker101@users.noreply.github.com>
2024-01-07 21:44:20 +0000

    Fix test

--- backend/Tests/Integration.Tests/Authentication/GivenAnUnAuthenticatedRequest.cs
+++ backend/Tests/Integration.Tests/Authentication/GivenAnUnAuthenticatedRequest.cs
@@ -1,6 +1,5 @@
 ﻿using ClickStitch.Api.Patterns.Types;
 using NUnit.Framework;
-using System.Net.Http.Json;
 
 namespace Integration.Tests.Authentication;
 
@@ -8,19 +7,17 @@ namespace Integration.Tests.Authentication;
 [Parallelizable]
 public sealed class GivenAnUnAuthenticatedRequest : IntegrationTest
 {
-    private GetPatternsResponse? _result;
+    private GetPatternsResponse _result = null!;
 
     [OneTimeSetUp]
     public async Task Setup()
     {
-        var response = await Client.GetAsync("api/patterns");
-
-        _result = await response.Content.ReadFromJsonAsync<GetPatternsResponse>();
+        _result = await DoRequest<GetPatternsResponse>(HttpMethod.Get, "api/patterns");
     }
 
     [Test]
     public void ThenTheCorrectPatternIsReturned()
     {
-        Assert.That(_result?.Patterns[0].Reference, Is.EqualTo(Guid.Parse("c3245143-c616-4b42-87dc-0ba20c52e4e2")));
+        Assert.That(_result.Patterns[0].Reference, Is.EqualTo(Guid.Parse("c3245143-c616-4b42-87dc-0ba20c52e4e2")));
     }
 }
