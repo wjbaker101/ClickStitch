ef689848545a28624a5209552057ec764bcf949c
William Baker <wjbaker101@users.noreply.github.com>
2024-01-13 17:33:54 +0000

    Collate users api files

--- frontend/src/api/api.ts
+++ frontend/src/api/api.ts
@@ -3,26 +3,21 @@ import axios from 'axios';
 import { useAuth } from '@/use/auth/Auth.use';
 import { ApiErrorMapper } from '@/api/ApiErrorMapper';
 
-import { userMapper } from '@/api/mappers/User.mapper';
-import { permissionMapper } from '@/api/mappers/Permission.mapper';
 import { threadMapper } from '@/api/mappers/Thread.mapper';
 
 import type { IApiResultResponse } from '@/api/api-models/ApiResponse.type';
 
-import type { IGetSelf } from '@/models/GetSelf.model';
-
-import type { ICreateUserRequest, ICreateUserResponse } from '@/api/types/CreateUser.type';
 import type { IInventoryThread } from '@/models/Inventory.model';
 import type { IGetInventoryThreadsResponse } from './types/GetInventoryThreads.type';
 import type { IUpdateInventoryThreadRequest, IUpdateInventoryThreadResponse } from './types/UpdateInventoryThread.type';
 import type { ISearchInventoryThreadsResponse } from './types/SearchInventoryThreads.type';
-import type { IGetSelfResponse } from './types/GetSelf.type';
 
 import { adminApi } from '@/api/parts/admin/admin.api';
 import { authApi } from '@/api/parts/auth/auth.api';
 import { creatorsApi } from '@/api/parts/creators/creators.api';
 import { patternsApi } from '@/api/parts/patterns/patterns.api';
 import { projectsApi } from '@/api/parts/projects/projects.api';
+import { usersApi } from '@/api/parts/users/users.api';
 
 const auth = useAuth();
 
@@ -42,43 +37,7 @@ export const api = {
 
     projects: projectsApi,
 
-    users: {
-
-        async getSelf(): Promise<IGetSelf | Error> {
-            if (auth.details.value === null)
-                return new Error('You must be logged in for this action.');
-
-            try {
-                const response = await client.get<IApiResultResponse<IGetSelfResponse>>('/users/self', {
-                    headers: {
-                        'Authorization': `Bearer ${auth.details.value.loginToken}`,
-                    },
-                });
-
-                const result = response.data.result;
-
-                return {
-                    user: userMapper.map(result.user),
-                    permissions: result.permissions.map(permissionMapper.map),
-                };
-            }
-            catch (error) {
-                return ApiErrorMapper.map(error);
-            }
-        },
-
-        async createUser(request: ICreateUserRequest): Promise<ICreateUserResponse | Error> {
-            try {
-                const response = await client.post<IApiResultResponse<ICreateUserResponse>>('/users', request);
-
-                return {};
-            }
-            catch (error) {
-                return ApiErrorMapper.map(error);
-            }
-        },
-
-    },
+    users: usersApi,
 
     inventory: {
 
