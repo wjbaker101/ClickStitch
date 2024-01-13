7bc0f3d25f043647278ab299b430097d495d4d49
William Baker <wjbaker101@users.noreply.github.com>
2024-01-13 13:35:12 +0000

    Formatting

--- Migrations/Migrations/Common/Names.cs
+++ Migrations/Migrations/Common/Names.cs
@@ -9,20 +9,23 @@ public static class Names
 
     public static class Tables
     {
-        public const string USER = "user";
+        public const string CREATOR = "creator";
         public const string PATTERN = "pattern";
-        public const string PATTERN_STITCH = "pattern_stitch";
         public const string PATTERN_THREAD = "pattern_thread";
         public const string PATTERN_THREAD_STITCH = "pattern_thread_stitch";
         public const string PERMISSION = "permission";
-        public const string USER_BASKET_ITEM = "user_basket_item";
+        public const string THREAD = "thread";
+        public const string USER = "user";
+        public const string USER_CREATOR = "user_creator";
         public const string USER_PATTERN = "user_pattern";
-        public const string USER_PATTERN_STITCH = "user_pattern_stitch";
         public const string USER_PATTERN_THREAD_STITCH = "user_pattern_thread_stitch";
         public const string USER_PERMISSION = "user_permission";
-        public const string CREATOR = "creator";
-        public const string USER_CREATOR = "user_creator";
-        public const string THREAD = "thread";
         public const string USER_THREAD = "user_thread";
+
+        #region Unused/Old Tables
+        public const string PATTERN_STITCH = "pattern_stitch";
+        public const string USER_BASKET_ITEM = "user_basket_item";
+        public const string USER_PATTERN_STITCH = "user_pattern_stitch";
+        #endregion
     }
 }
