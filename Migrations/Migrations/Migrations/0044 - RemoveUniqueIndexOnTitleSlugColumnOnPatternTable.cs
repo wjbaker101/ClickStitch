62a6e1f4a3749dd03d55f27e0f4a090bf747bfe0
William Baker <wjbaker101@users.noreply.github.com>
2024-01-13 13:24:55 +0000

    Allow stitchers to create patterns of the same name

---
+++ Migrations/Migrations/Migrations/0044 - RemoveUniqueIndexOnTitleSlugColumnOnPatternTable.cs
@@ -0,0 +1,42 @@
+﻿using FluentMigrator;
+using Migrations.Common;
+
+namespace Migrations.Migrations;
+
+// ReSharper disable once InconsistentNaming
+[Migration(0044)]
+public sealed class RemoveUniqueIndexOnTitleSlugColumnOnPatternTable_0044 : Migration
+{
+    public override void Up()
+    {
+        Delete
+            .Index()
+            .OnTable(Names.Tables.PATTERN)
+            .InSchema(Names.Schemas.CLICK_STITCH)
+            .OnColumn("title_slug");
+
+        Create
+            .Index()
+            .OnTable(Names.Tables.PATTERN)
+            .InSchema(Names.Schemas.CLICK_STITCH)
+            .OnColumn("title_slug");
+    }
+
+    public override void Down()
+    {
+        Delete
+            .Index()
+            .OnTable(Names.Tables.PATTERN)
+            .InSchema(Names.Schemas.CLICK_STITCH)
+            .OnColumn("title_slug");
+
+        Create
+            .Index()
+            .OnTable(Names.Tables.PATTERN)
+            .InSchema(Names.Schemas.CLICK_STITCH)
+            .OnColumn("title_slug")
+            .Ascending()
+            .WithOptions()
+            .Unique();
+    }
+}
