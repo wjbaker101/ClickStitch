bf52323115b8dd426c074de1358306283d917759
William Baker <wjbaker101@users.noreply.github.com>
2024-01-15 22:53:19 +0000

    Move classes into their own files

---
+++ backend/Tests/TestHelpers/Data/TestApiSession.cs
@@ -0,0 +1,56 @@
+﻿using Data.Types;
+
+namespace TestHelpers.Data;
+
+public sealed class TestApiSession : IApiSession
+{
+    private readonly List<IDatabaseRecord> _records;
+    private readonly DatabaseActionsListener _actions;
+
+    public TestApiSession(List<IDatabaseRecord> records, DatabaseActionsListener actions)
+    {
+        _records = records;
+        _actions = actions;
+    }
+
+    public Task<IApiTransaction> BeginTransaction(CancellationToken cancellationToken)
+    {
+        return Task.FromResult<IApiTransaction>(new TestApiTransaction());
+    }
+
+    public Task<TRecord> Load<TRecord>(long id, CancellationToken cancellationToken) where TRecord : IDatabaseRecordWithId
+    {
+        return Task.FromResult(_records.OfType<TRecord>().First(x => x.Id == id));
+    }
+
+    public IApiQueryable<TRecord> Query<TRecord>() where TRecord : IDatabaseRecord
+    {
+        return new TestApiQueryable<TRecord>(_records.OfType<TRecord>().AsQueryable());
+    }
+
+    public Task<TRecord> Save<TRecord>(TRecord record, CancellationToken cancellationToken) where TRecord : IDatabaseRecord
+    {
+        _records.Add(record);
+        _actions.OnSave(record);
+
+        return Task.FromResult(record);
+    }
+
+    public Task<TRecord> Update<TRecord>(TRecord record, CancellationToken cancellationToken) where TRecord : IDatabaseRecord
+    {
+        _actions.OnUpdate(record);
+        return Task.FromResult(record);
+    }
+
+    public Task Delete<TRecord>(TRecord record, CancellationToken cancellationToken) where TRecord : IDatabaseRecord
+    {
+        _records.Remove(record);
+        _actions.OnDelete(record);
+
+        return Task.FromResult(record);
+    }
+
+    public void Dispose()
+    {
+    }
+}
---
+++ backend/Tests/TestHelpers/Data/TestApiTransaction.cs
@@ -0,0 +1,15 @@
+﻿using Data.Types;
+
+namespace TestHelpers.Data;
+
+public sealed class TestApiTransaction : IApiTransaction
+{
+    public Task Commit(CancellationToken cancellationToken)
+    {
+        return Task.CompletedTask;
+    }
+
+    public void Dispose()
+    {
+    }
+}
