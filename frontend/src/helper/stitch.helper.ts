3a6443f703d38558464631c517d8346762b8011c
William Baker <wjbaker101@users.noreply.github.com>
2024-01-22 22:46:02 +0000

    Create method for calculating the number of skeins required for pattern threads

--- frontend/src/helper/stitch.helper.ts
+++ frontend/src/helper/stitch.helper.ts
@@ -44,3 +44,11 @@ export const calculateFabricSize = function (width: number, height: number, aida
         },
     };
 };
+
+export const calculateRequiredSkeins = function (stitches: number, aidaCount: number): number {
+    // https://www.mismatch.co.uk/cross.htm#floss_amt
+    const strands = 2;
+    const estimatedStitches = 17 * (15 / (6 / aidaCount)) * (6 / strands);
+
+    return Math.ceil(stitches / estimatedStitches);
+};
