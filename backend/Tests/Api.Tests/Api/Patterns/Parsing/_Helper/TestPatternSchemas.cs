2d935868f3c31b32055faed1a38035f01ecdc3d2
William Baker <wjbaker101@users.noreply.github.com>
2024-01-26 23:23:15 +0000

    Create test for parsing certain schemas

---
+++ backend/Tests/Api.Tests/Api/Patterns/Parsing/_Helper/TestPatternSchemas.cs
@@ -0,0 +1,6 @@
+﻿namespace Api.Tests.Api.Patterns.Parsing._Helper;
+
+public static class TestPatternSchemas
+{
+    public static string FcJson() => File.ReadAllText(Path.Combine(TestContext.CurrentContext.TestDirectory, "Api/Patterns/Parsing/_Helper", "test.fcjson"));
+}
