c6a7e788b89d9dfc3d4cb4617c3754219e8eb03a
William Baker <wjbaker101@users.noreply.github.com>
2024-01-27 10:39:09 +0000

    Delete unused api calls

--- frontend/src/api/parts/admin/admin.api.ts
+++ frontend/src/api/parts/admin/admin.api.ts
@@ -3,19 +3,14 @@ import { apiClient } from '@/api/client';
 import { paginationMapper } from '@/api/mappers/Pagination.mapper';
 import { userMapper } from '@/api/mappers/User.mapper';
 import { permissionMapper } from '@/api/mappers/Permission.mapper';
-import { threadMapper } from '@/api/mappers/Thread.mapper';
 
 import type { IPermission } from '@/models/Permission.model';
 import type { IGetUsers } from '@/models/GetUsers.model';
-import type { IThread } from '@/models/Thread.model';
 
 import type { IGetUsersResponse } from '@/api/parts/admin/types/GetUsers.type';
 import type { IGetPermissionsResponse } from '@/api/parts/admin/types/GetPermissions.type';
 import type { IAssignPermissionToUserRequest, IAssignPermissionToUserResponse } from '@/api/parts/admin/types/AssignPermissionToUser.type';
 import type { IRemovePermissionFromUserResponse } from '@/api/parts/admin/types/RemovePermissionFromUser.type';
-import type { ICreateThreadRequest, ICreateThreadResponse } from '@/api/parts/admin/types/CreateThread.type';
-import type { IUpdateThreadRequest, IUpdateThreadResponse } from '@/api/parts/admin/types/UpdateThread.type';
-import type { IDeleteThreadResponse } from '@/api/parts/admin/types/DeleteThread.type';
 
 export const adminApi = {
 
@@ -77,51 +72,6 @@ export const adminApi = {
     async removePermissionFromUser(userReference: string, permissionType: number): Promise<void | Error> {
         const response = await apiClient.delete<IRemovePermissionFromUserResponse>({
             url: `/admin/users/${userReference}/permissions/${permissionType}`,
-            auth: {
-                use: true,
-                required: true,
-            },
-        });
-
-        if (response instanceof Error)
-            return response;
-    },
-
-    async createThread(request: ICreateThreadRequest): Promise<IThread | Error> {
-        const response = await apiClient.post<ICreateThreadResponse>({
-            url: '/admin/threads',
-            body: request,
-            auth: {
-                use: true,
-                required: true,
-            },
-        });
-
-        if (response instanceof Error)
-            return response;
-
-        return threadMapper.map(response.thread);
-    },
-
-    async updateThread(threadReference: string, request: IUpdateThreadRequest): Promise<IThread | Error> {
-        const response = await apiClient.put<IUpdateThreadResponse>({
-            url: `/admin/threads/${threadReference}`,
-            body: request,
-            auth: {
-                use: true,
-                required: true,
-            },
-        });
-
-        if (response instanceof Error)
-            return response;
-
-        return threadMapper.map(response.thread);
-    },
-
-    async deleteThread(threadReference: string): Promise<void | Error> {
-        const response = await apiClient.delete<IDeleteThreadResponse>({
-            url: `/admin/threads/${threadReference}`,
             auth: {
                 use: true,
                 required: true,
