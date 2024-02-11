17e5d263bf25adf07f4fb9d2dac28b685d4f362c
William Baker <wjbaker101@users.noreply.github.com>
2024-02-11 12:40:16 +0000

    Unused

--- frontend/src/data/data.ts
+++
@@ -1,5 +0,0 @@
-export const instagramUrl = 'https://www.instagram.com/ClickStitchStudio/';
-
-export const twitterUrl = 'https://twitter.com/ClickStitchApp';
-
-export const lemmyUrl = 'https://sh.itjust.works/u/ClickStitch?page=1&sort=New&view=Posts';
