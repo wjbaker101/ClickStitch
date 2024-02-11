fcb69adb39a6c2cbd047872e89c9b5e408b0f2e0
William Baker <wjbaker101@users.noreply.github.com>
2024-02-11 12:53:23 +0000

    Unused

--- frontend/src/helper/helper.ts
+++ frontend/src/helper/helper.ts
@@ -1,10 +1,3 @@
-export const currency = function (price: number): string {
-    return new Intl.NumberFormat('en-GB', {
-        style: 'currency',
-        currency: 'gbp',
-    }).format(price);
-};
-
 export const formatNumber = function (value: number): string {
     return new Intl.NumberFormat('en-GB').format(value);
 };
@@ -21,8 +14,4 @@ export const isDark = function (colour: string): boolean {
     const lightness = 0.2126 * red + 0.7152 * green + 0.0722 * blue; // ITU-R BT.709
 
     return lightness < 90;
-};
-
-export const sum = function <T>(numbers: Array<T>, func: (t: T) => number): number {
-    return numbers.reduce((total, x) => total + func(x), 0);
 };
