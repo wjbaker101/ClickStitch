ed4573d4d9f3eeb2c806819b57ba63cbc1d748d9
William Baker <wjbaker101@users.noreply.github.com>
2024-02-11 21:08:03 +0000

    Add reference to projects so that they can be linked to externally without having to pass through userReference+patternReference, preserving some anonyminity for users and making urls smaller

---
+++ Migrations/Migrations/Migrations/0046 - AddReferenceColumnToUserPatternTable.cs
@@ -0,0 +1,26 @@
+﻿using FluentMigrator;
+using Migrations.Common;
+
+namespace Migrations.Migrations;
+
+// ReSharper disable once InconsistentNaming
+[Migration(0046)]
+public sealed class AddReferenceColumnToUserPatternTable_0046 : AutoReversingMigration
+{
+    public override void Up()
+    {
+        Alter
+            .Table(Names.Tables.USER_PATTERN)
+            .InSchema(Names.Schemas.CLICK_STITCH)
+            .AddColumn("reference")
+            .AsGuid()
+            .Nullable();
+
+        Create
+            .Index()
+            .OnTable(Names.Tables.USER_PATTERN)
+            .InSchema(Names.Schemas.CLICK_STITCH)
+            .OnColumn("reference")
+            .Unique();
+    }
+}
