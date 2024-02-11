6c085cc5966c14a15ea716a1f71f26e9c0295963
William Baker <wjbaker101@users.noreply.github.com>
2024-02-11 12:42:26 +0000

    Cleanup

--- frontend/src/api/api.ts
+++ frontend/src/api/api.ts
@@ -1,19 +1,19 @@
 import { adminApi } from '@/api/parts/admin/admin.api';
 import { authApi } from '@/api/parts/auth/auth.api';
 import { creatorsApi } from '@/api/parts/creators/creators.api';
+import { inventoryApi } from '@/api/parts/inventory/inventory.api';
 import { patternsApi } from '@/api/parts/patterns/patterns.api';
 import { projectsApi } from '@/api/parts/projects/projects.api';
 import { usersApi } from '@/api/parts/users/users.api';
-import { inventoryApi } from './parts/inventory/inventory.api';
 
 export const api = {
 
     admin: adminApi,
     auth: authApi,
     creators: creatorsApi,
+    inventory: inventoryApi,
     patterns: patternsApi,
     projects: projectsApi,
     users: usersApi,
-    inventory: inventoryApi,
 
 };
