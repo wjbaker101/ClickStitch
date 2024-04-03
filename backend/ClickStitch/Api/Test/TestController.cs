ec2a39072cee862c721cfc0af23118aff9a2640a
William Baker <wjbaker101@users.noreply.github.com>
2024-04-03 23:57:34 +0100

    Test

--- backend/ClickStitch/Api/Test/TestController.cs
+++ backend/ClickStitch/Api/Test/TestController.cs
@@ -1,4 +1,5 @@
-﻿using DotNetLibs.Api.Types;
+﻿using ClickStitch.Middleware.Authentication;
+using DotNetLibs.Api.Types;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ClickStitch.Api.Test;
@@ -8,6 +9,7 @@ public sealed class TestController : ApiController
 {
     [HttpGet]
     [Route("")]
+    [Authenticate]
     public IActionResult ThrowException()
     {
         throw new Exception("Throwing an exception...");
