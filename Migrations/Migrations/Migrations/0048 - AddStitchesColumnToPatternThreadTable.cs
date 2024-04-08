5d8c752cfb183652ab30c77b2703669e02247cd5
William Baker <wjbaker101@users.noreply.github.com>
2024-04-08 20:46:37 +0100

    Add stitches column to pattern_thread. This will be a faster way of retriving stitches of projects and will make it easier to create delete functionality.
    Currently in order to delete user_stitches, you must know the stitch id. This is very inefficient, so only saving the x/y/stitchedAt so that lookup logic is done in-memory and so they can be deleted by their user_thread id, or at least something more efficient to select.

---
+++ Migrations/Migrations/Migrations/0048 - AddStitchesColumnToPatternThreadTable.cs
@@ -0,0 +1,19 @@
+﻿using FluentMigrator;
+using Migrations.Common;
+
+namespace Migrations.Migrations;
+
+// ReSharper disable once InconsistentNaming
+[Migration(0048)]
+public sealed class AddStitchesColumnToPatternThreadTable_0048 : AutoReversingMigration
+{
+    public override void Up()
+    {
+        Alter
+            .Table(Names.Tables.PATTERN_THREAD)
+            .InSchema(Names.Schemas.CLICK_STITCH)
+            .AddColumn("stitches")
+            .AsCustom("json")
+            .Nullable();
+    }
+}
