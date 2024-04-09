17533970b2f77ad29a55d6fcefd3fc84148a2b05
William Baker <wjbaker101@users.noreply.github.com>
2024-04-09 18:50:47 +0100

    Set these columns to not nullable now that they have been backfilled

---
+++ Migrations/Migrations/Migrations/0051 - MakeNewPatternThreadStitchColumnsNotNullable.cs
@@ -0,0 +1,29 @@
+﻿using FluentMigrator;
+using Migrations.Common;
+using Migrations.Extensions;
+
+namespace Migrations.Migrations;
+
+// ReSharper disable once InconsistentNaming
+[Migration(0051)]
+public sealed class MakeNewPatternThreadStitchColumnsNotNullable_0051 : AutoReversingMigration
+{
+    public override void Up()
+    {
+        Alter
+            .Table(Names.Tables.USER_PATTERN_THREAD_STITCH)
+            .InSchema(Names.Schemas.CLICK_STITCH)
+            .AlterColumn("pattern_thread_id")
+            .AsInt64()
+            .NotNullable()
+            .AlterColumn("x")
+            .AsInt32()
+            .NotNullable()
+            .AlterColumn("y")
+            .AsInt32()
+            .NotNullable()
+            .AlterColumn("completed_at")
+            .AsTimestampWithTimeZone()
+            .NotNullable(); 
+    }
+}
