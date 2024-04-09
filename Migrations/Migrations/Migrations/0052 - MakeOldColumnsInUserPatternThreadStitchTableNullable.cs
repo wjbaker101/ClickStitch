9097cc4bac4f553937bdb35fd129a4a64d63f25f
William Baker <wjbaker101@users.noreply.github.com>
2024-04-09 19:57:56 +0100

    Set old columns in the user_pattern_thread_stitch table nullable so that they can eventually be deleted

---
+++ Migrations/Migrations/Migrations/0052 - MakeOldColumnsInUserPatternThreadStitchTableNullable.cs
@@ -0,0 +1,23 @@
+﻿using FluentMigrator;
+using Migrations.Common;
+using Migrations.Extensions;
+
+namespace Migrations.Migrations;
+
+// ReSharper disable once InconsistentNaming
+[Migration(0052)]
+public sealed class MakeOldColumnsInUserPatternThreadStitchTableNullable_0052 : AutoReversingMigration
+{
+    public override void Up()
+    {
+        Alter
+            .Table(Names.Tables.USER_PATTERN_THREAD_STITCH)
+            .InSchema(Names.Schemas.CLICK_STITCH)
+            .AlterColumn("pattern_thread_stitch_id")
+            .AsInt64()
+            .Nullable()
+            .AlterColumn("stitched_at")
+            .AsTimestampWithTimeZone()
+            .Nullable();
+    }
+}
