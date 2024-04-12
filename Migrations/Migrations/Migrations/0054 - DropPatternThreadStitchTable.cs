dcf39a36be77a7b95274d1bb80605ca52ed74964
William Baker <wjbaker101@users.noreply.github.com>
2024-04-12 19:50:43 +0100

    Delete now unused table

---
+++ Migrations/Migrations/Migrations/0054 - DropPatternThreadStitchTable.cs
@@ -0,0 +1,20 @@
+﻿using FluentMigrator;
+using Migrations.Common;
+
+namespace Migrations.Migrations;
+
+// ReSharper disable once InconsistentNaming
+[Migration(0054)]
+public sealed class DropPatternThreadStitchTable_0054 : Migration
+{
+    public override void Up()
+    {
+        Delete
+            .Table(Names.Tables.PATTERN_THREAD_STITCH)
+            .InSchema(Names.Schemas.CLICK_STITCH);
+    }
+
+    public override void Down()
+    {
+    }
+}
