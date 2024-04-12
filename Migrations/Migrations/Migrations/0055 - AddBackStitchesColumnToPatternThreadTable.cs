d61c34fdb8115febd5cecc7154a653322d340775
William Baker <wjbaker101@users.noreply.github.com>
2024-04-12 19:56:52 +0100

    Add backstitches column for new feature

---
+++ Migrations/Migrations/Migrations/0055 - AddBackStitchesColumnToPatternThreadTable.cs
@@ -0,0 +1,19 @@
+﻿using FluentMigrator;
+using Migrations.Common;
+
+namespace Migrations.Migrations;
+
+// ReSharper disable once InconsistentNaming
+[Migration(0055)]
+public sealed class AddBackStitchesColumnToPatternThreadTable_0055 : AutoReversingMigration
+{
+    public override void Up()
+    {
+        Alter
+            .Table(Names.Tables.PATTERN_THREAD)
+            .InSchema(Names.Schemas.CLICK_STITCH)
+            .AddColumn("back_stitches")
+            .AsCustom("json")
+            .Nullable();
+    }
+}
