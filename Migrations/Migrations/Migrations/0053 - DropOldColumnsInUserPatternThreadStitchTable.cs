5c3b7fb12de3e7d53509efe5e6816f4dac7d3f92
William Baker <wjbaker101@users.noreply.github.com>
2024-04-12 19:49:09 +0100

    Drop now unused columns

---
+++ Migrations/Migrations/Migrations/0053 - DropOldColumnsInUserPatternThreadStitchTable.cs
@@ -0,0 +1,26 @@
+﻿using FluentMigrator;
+using Migrations.Common;
+
+namespace Migrations.Migrations;
+
+// ReSharper disable once InconsistentNaming
+[Migration(0053)]
+public sealed class DropOldColumnsInUserPatternThreadStitchTable_0053 : Migration
+{
+    public override void Up()
+    {
+        Delete
+            .Column("pattern_thread_stitch_id")
+            .FromTable(Names.Tables.USER_PATTERN_THREAD_STITCH)
+            .InSchema(Names.Schemas.CLICK_STITCH);
+
+        Delete
+            .Column("stitched_at")
+            .FromTable(Names.Tables.USER_PATTERN_THREAD_STITCH)
+            .InSchema(Names.Schemas.CLICK_STITCH);
+    }
+
+    public override void Down()
+    {
+    }
+}
