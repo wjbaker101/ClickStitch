39383fcdeaa9361ee9e701c25fa06e3303de44ff
William Baker <wjbaker101@users.noreply.github.com>
2024-04-25 20:53:51 +0100

    Render backstitches

--- frontend/src/views/stitcher/project/layers/BackStitchesLayer.component.vue
+++ frontend/src/views/stitcher/project/layers/BackStitchesLayer.component.vue
@@ -26,9 +26,22 @@ const { graphics } = useCanvasElement(canvas);
 const render = function () {
     const threads = project.value.threads;
 
+    graphics.value.lineWidth = props.baseStitchSize / 2;
+    graphics.value.lineCap = 'round';
+    graphics.value.shadowBlur = 3;
+
     for (const thread of threads) {
-        console.log(thread.backStitches)
+        graphics.value.strokeStyle = thread.thread.colour;
+        graphics.value.shadowColor = thread.thread.colour;
+
+        for (const backStitch of thread.backStitches) {
+            graphics.value.beginPath();
+            graphics.value.moveTo(backStitch[0] * props.baseStitchSize, backStitch[1] * props.baseStitchSize);
+            graphics.value.lineTo(backStitch[2] * props.baseStitchSize, backStitch[3] * props.baseStitchSize);
+            graphics.value.stroke();
+        }
     }
+
 };
 
 onMounted(() => {
