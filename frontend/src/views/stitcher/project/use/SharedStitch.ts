bf072dbceb66229bee24057ba5c0844849e9ee23
William Baker <wjbaker101@users.noreply.github.com>
2024-05-15 22:40:43 +0100

    Unused

--- frontend/src/views/stitcher/project/use/SharedStitch.ts
+++ frontend/src/views/stitcher/project/use/SharedStitch.ts
@@ -3,11 +3,9 @@ import { ref } from 'vue';
 import { type IStitch } from '@/models/Pattern.model';
 
 const hoveredStitch = ref<IStitch | null>(null);
-const percentageCompleted = ref<number | null>(null);
 
 export const useSharedStitch = function () {
     return {
         hoveredStitch,
-        percentageCompleted,
     };
 };
