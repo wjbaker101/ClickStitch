542bbe6a4d66a8efc472dde6e823f91a770041ff
William Baker <wjbaker101@users.noreply.github.com>
2024-05-24 00:03:12 +0100

    Unused

--- frontend/src/helper/stitch.helper.ts
+++ frontend/src/helper/stitch.helper.ts
@@ -1,31 +1,3 @@
-export interface IFabricSize {
-    in: ISize;
-    cm: ISize;
-}
-
-interface ISize {
-    readonly width: number;
-    readonly height: number;
-}
-
-export const calculateFabricSize = function (width: number, height: number, aidaCount: number): IFabricSize {
-    const padding = 2;
-
-    const widthInches = width / aidaCount + padding * 2;
-    const heightInches = height / aidaCount + padding * 2;
-
-    return {
-        in: {
-            width: Math.ceil(widthInches),
-            height: Math.ceil(heightInches),
-        },
-        cm: {
-            width: Math.ceil(widthInches * 2.54),
-            height: Math.ceil(heightInches * 2.54),
-        },
-    };
-};
-
 export const calculateRequiredSkeins = function (stitches: number, aidaCount: number): number {
     // https://www.mismatch.co.uk/cross.htm#floss_amt
     const strands = 2;
