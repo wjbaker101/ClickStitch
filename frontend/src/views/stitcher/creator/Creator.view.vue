bf45c43e44e246939ca340648ae19977ce34fe32
William Baker <wjbaker101@users.noreply.github.com>
2024-05-23 20:04:03 +0100

    Add the start of a new view that will display information about a particular creator

---
+++ frontend/src/views/stitcher/creator/Creator.view.vue
@@ -0,0 +1,15 @@
+<template>
+    <ViewComponent class="creator-view">
+        <template #nav>
+            <strong>Creator</strong>
+        </template>
+    </ViewComponent>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style lang="scss">
+.creator-view {
+}
+</style>
