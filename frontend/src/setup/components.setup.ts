f6b422523dc7ee2558cf7ec1f6dd823f0c2eb487
William Baker <wjbaker101@users.noreply.github.com>
2024-05-24 23:12:47 +0100

    Possibly might want to move away from @wjb/vue so ClickStitch is more isolated. This also gives the potential to using Tailwind for CSS

--- frontend/src/setup/components.setup.ts
+++ frontend/src/setup/components.setup.ts
@@ -1,6 +1,14 @@
 import { type Plugin } from 'vue';
 
-import { components as wjbComponents } from '@wjb/vue/setup/components';
+import ButtonComponent from '@wjb/vue/component/ButtonComponent.vue';
+import DeleteButtonComponent from '@wjb/vue/component/DeleteButtonComponent.vue';
+import IconComponent from '@wjb/vue/component/IconComponent.vue';
+import LoadingComponent from '@wjb/vue/component/LoadingComponent.vue';
+import ModalComponent from '@wjb/vue/component/ModalComponent.vue';
+import PopupComponent from '@wjb/vue/component/PopupComponent.vue';
+import FormComponent from '@wjb/vue/component/form/FormComponent.vue';
+import FormInputComponent from '@wjb/vue/component/form/FormInputComponent.vue';
+import FormSectionComponent from '@wjb/vue/component/form/FormSectionComponent.vue';
 
 import CardComponent from '@/components/CardComponent.vue';
 import LinkComponent from '@/components/LinkComponent.vue';
@@ -9,7 +17,16 @@ import ViewComponent from '@/components/ViewComponent.vue';
 export const components: Plugin = {
 
     install(app) {
-        app.use(wjbComponents);
+        app.component('ButtonComponent', ButtonComponent);
+        app.component('DeleteButtonComponent', DeleteButtonComponent);
+        app.component('IconComponent', IconComponent);
+        app.component('LoadingComponent', LoadingComponent);
+        app.component('ModalComponent', ModalComponent);
+        app.component('PopupComponent', PopupComponent);
+
+        app.component('FormComponent', FormComponent);
+        app.component('FormInputComponent', FormInputComponent);
+        app.component('FormSectionComponent', FormSectionComponent);
 
         app.component('CardComponent', CardComponent);
         app.component('LinkComponent', LinkComponent);
