23d07d69984c452418c6a8e00f0574fcfff15db3
William Baker <wjbaker101@users.noreply.github.com>
2024-05-25 11:43:56 +0100

    Display pattern count

--- frontend/src/views/stitcher/creator/CreatorView.vue
+++ frontend/src/views/stitcher/creator/CreatorView.vue
@@ -5,19 +5,24 @@
         </template>
         <LoadingComponent v-if="isLoading" itemName="creator" />
         <div v-else-if="creator !== null" class="content-width">
-            <CardComponent border="top" padded>
-                <h2>
-                    <IconComponent icon="user" size="large" gap="right" />
-                    <span class="name">{{ creator.name }}</span>
-                </h2>
-                <p>
-                    Creator since: {{ creator.createdAt.format('MMMM YYYY') }}
-                </p>
-                <LinkComponent :href="creator.storeUrl">
-                    <ButtonComponent>
-                        Visit their Shop!
-                    </ButtonComponent>
-                </LinkComponent>
+            <CardComponent border="top" padded class="flex gap align-items-center">
+                <div>
+                    <h2>
+                        <IconComponent icon="user" size="large" gap="right" />
+                        <span class="name">{{ creator.name }}</span>
+                    </h2>
+                    <p>
+                        Creator since: {{ creator.createdAt.format('MMMM YYYY') }}
+                    </p>
+                    <LinkComponent :href="creator.storeUrl">
+                        <ButtonComponent>
+                            Visit their Shop!
+                        </ButtonComponent>
+                    </LinkComponent>
+                </div>
+                <div class="flex-auto">
+                    <div class="total-count">{{ totalPatternCount }}</div> patterns
+                </div>
             </CardComponent>
             <PaginatedContentComponent loadingItemName="pattens" :pageSize="10" :logic="loadPatterns">
                 <div class="patterns">
@@ -50,7 +55,7 @@ const isLoading = ref<boolean>(false);
 
 const isPatternsLoading = ref<boolean>(false);
 const patterns = ref<Array<IPattern>>([]);
-
+const totalPatternCount = ref<number | null>(null);
 const projectPatternReferencesForUser = ref<Array<string>>([]);
 
 const loadPatterns = async function (pageNumber: number, pageSize: number): Promise<IPagination | Error> {
@@ -63,6 +68,7 @@ const loadPatterns = async function (pageNumber: number, pageSize: number): Prom
 
     patterns.value = response.patterns;
     projectPatternReferencesForUser.value = response.projectPatternReferencesForUser;
+    totalPatternCount.value = response.pagination.totalCount;
 
     return response.pagination;
 };
@@ -94,6 +100,19 @@ onBeforeMount(async () => {
         display: grid;
         gap: 3rem;
         grid-template-columns: repeat(auto-fill, minmax(min(350px, 100%), 1fr));
+    }
+
+    .total-count {
+        width: 2.5rem;
+        aspect-ratio: 1;
+        display: inline-block;
+        padding: 0.5rem;
+        margin-right: 0.25rem;
+        background-color: var(--wjb-secondary);
+        border-radius: 50%;
+        color: var(--wjb-light);
+        text-align: center;
+        box-shadow: 0 2px 12px rgba(0, 0, 0, 0.1), 0 6px 16px -12px rgba(0, 0, 0, 1);
     }
 }
 </style>
