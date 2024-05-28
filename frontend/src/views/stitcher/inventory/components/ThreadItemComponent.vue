ffad1d683aa8447fa970ddb4e9feb0456749aaf3
William Baker <wjbaker101@users.noreply.github.com>
2024-05-28 23:26:53 +0100

    Use Tailwind

--- frontend/src/views/stitcher/inventory/components/ThreadItemComponent.vue
+++ frontend/src/views/stitcher/inventory/components/ThreadItemComponent.vue
@@ -1,14 +1,14 @@
 <template>
-    <ListItemComponent class="thread-item-component" :style="{ '--colour': thread.thread.colour }" :class="{ 'is-dark': isDark(thread.thread.colour) }">
-        <div class="flex gap align-items-center">
-            <div class="icon-container flex-auto">
-                <IconComponent icon="skein" size="large" />
+    <ListItemComponent :style="{ '--colour': thread.thread.colour }" :class="{ 'is-dark': isDark(thread.thread.colour) }" class="shadow-md thread-item-component text-dark [&.is-dark]:text-light">
+        <div class="flex items-center gap-4">
+            <div class="flex-auto p-2 rounded-full bg-[var(--colour)] text-inherit shadow-md">
+                <IconComponent icon="skein" size="large" class="size-8" />
             </div>
             <div>
                 <strong>{{ thread.thread.brand }} {{ thread.thread.code }}</strong>
             </div>
             <div class="flex-auto">
-                <input class="counter" type="number" min="0" max="9999" step="1" v-model="count">
+                <input type="number" min="0" max="9999" step="1" v-model="count" class="w-28">
             </div>
         </div>
     </ListItemComponent>
@@ -47,32 +47,5 @@ watchDebounced(count, async () => {
 <style lang="scss">
 .thread-item-component {
     background: linear-gradient(to right, var(--colour), var(--wjb-background-colour-light));
-    box-shadow: 0 2px 12px rgba(0, 0, 0, 0.1), 0 6px 16px -12px rgba(0, 0, 0, 1);
-    color: var(--wjb-dark);
-
-    .thread-image {
-        padding: 1rem;
-    }
-
-    .counter {
-        width: 7rem;
-    }
-
-    .icon-container {
-        padding: 0.5rem;
-        border-radius: 50%;
-        background-color: var(--colour);
-        color: inherit;
-        box-shadow: 1px 2px 6px rgba(0, 0, 0, 0.3);
-
-        .icon-skein {
-            width: 2rem;
-            height: 2rem;
-        }
-    }
-
-    &.is-dark {
-        color: var(--wjb-light);
-    }
 }
 </style>
