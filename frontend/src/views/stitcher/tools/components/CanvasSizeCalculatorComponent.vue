145d754aef928e4b2428f7a4bbccc8939fc58aa8
William Baker <wjbaker101@users.noreply.github.com>
2024-05-29 16:54:09 +0100

    Use Tailwind

--- frontend/src/views/stitcher/tools/components/CanvasSizeCalculatorComponent.vue
+++ frontend/src/views/stitcher/tools/components/CanvasSizeCalculatorComponent.vue
@@ -1,6 +1,6 @@
 <template>
     <h2>Canvas Size Calculator</h2>
-    <div class="canvas-size-calculator-component">
+    <div class="grid items-center gap-4 canvas-size-calculator-component sm:grid-cols-[1fr_auto_1fr] md:grid-cols-[1fr_auto_1fr_1fr]">
         <FormComponent>
             <FormSectionComponent>
                 <FormInputComponent label="Width (Stitches)">
@@ -14,9 +14,9 @@
                 </FormInputComponent>
             </FormSectionComponent>
         </FormComponent>
-        <div class="flex-auto text-centered">
-            <IconComponent class="arrow-right" icon="arrow-right" />
-            <IconComponent class="arrow-down" icon="arrow-down" />
+        <div class="flex-auto text-center">
+            <IconComponent class="hidden sm:block" icon="arrow-right" />
+            <IconComponent class="sm:hidden" icon="arrow-down" />
         </div>
         <FormComponent>
             <FormSectionComponent>
@@ -33,8 +33,8 @@
             </FormSectionComponent>
             <div class="text-centered">Make sure to add a few cm/inches as padding</div>
         </FormComponent>
-        <div class="preview-container">
-            <div class="preview" :style="style">
+        <div class="m-auto flex place-content-center preview-container h-[200px] sm:col-start-1 sm:col-end-4 md:col-start-4 md:col-end-4">
+            <div class="m-auto flex flex-auto items-center rounded-md border-2 border-solid text-center preview border-secondary" :style="style">
                 <small>Preview</small>
             </div>
         </div>
@@ -73,53 +73,9 @@ const style = computed<Record<string, string>>(() => {
 
 <style lang="scss">
 .canvas-size-calculator-component {
-    display: grid;
-    gap: 1rem;
-    grid-template-columns: 1fr auto 1fr 1fr;
-    align-items: center;
-
-    .arrow-down {
-        display: none;
-    }
-
-    .preview-container {
-        height: 200px;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        margin: auto;
-    }
-
     .preview {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        margin: auto;
-        border: 1px solid var(--wjb-secondary);
-        border-radius: var(--wjb-border-radius);
         background-color: var(--wjb-background-colour-light);
         background-image: var(--stitched-background-image);
-    }
-
-    @media screen and (max-width: 800px) {
-        grid-template-columns: 1fr auto 1fr;
-        grid-template-rows: auto auto;
-
-        .preview-container {
-            grid-column: 1 / 4;
-        }
-    }
-
-    @media screen and (max-width: 600px) {
-        display: block;
-
-        .arrow-down {
-            display: unset;
-        }
-
-        .arrow-right {
-            display: none;
-        }
     }
 }
 </style>
