0f978f7b72ed38523502c410544122c4453064c7
William Baker <wjbaker101@users.noreply.github.com>
2024-05-31 23:56:56 +0100

    Use Tailwind

--- frontend/src/components/CardComponent.vue
+++ frontend/src/components/CardComponent.vue
@@ -1,15 +1,15 @@
 <template>
     <div
-        class="rounded-md shadow-xl card-component"
+        class="rounded-md border-0 border-solid shadow-xl outline-dashed outline-2 outline-offset-1 outline-transparent card-component bg-background border-primary py-8 px-4
+        [&.border-top]:border-t-2 [&.border-top]:rounded-t-none
+        [&.border-bottom]:border-b-2 [&.border-bottom]:rounded-b-none
+        [&.border-left]:border-l-2 [&.border-left]:rounded-l-none
+        [&.border-right]:border-r-2 [&.border-right]:rounded-r-none"
         :class="{
-            [`border-${border}`]: true,
-            'is-hoverable': hoverable,
+            [`border-${border}`]: border,
             'is-padded': padded,
         }"
     >
-        <header v-if="header">
-            <slot name="header"></slot>
-        </header>
         <slot></slot>
     </div>
 </template>
@@ -17,31 +17,14 @@
 <script setup lang="ts">
 defineProps<{
     border?: 'top' | 'right' | 'bottom' | 'left';
-    hoverable?: boolean;
     padded?: boolean;
-    header?: boolean;
 }>();
 </script>
 
 <style lang="scss">
 .card-component {
-    background-color: var(--wjb-background-colour);
-    border-width: 0;
-    border-style: solid;
-    border-color: var(--wjb-primary);
-    outline: 2px dashed transparent;
-    outline-offset: 1px;
-
-    &.is-hoverable {
-        cursor: pointer;
-
-        &:hover {
-            outline: 2px dashed var(--wjb-tertiary);
-        }
-    }
 
     &.is-padded {
-        padding: 2rem 1rem;
 
         & > *:first-child {
             margin-top: 0;
@@ -49,55 +32,6 @@ defineProps<{
 
         & > *:last-child {
             margin-bottom: 0;
-        }
-    }
-
-    &.border-top {
-        border-top-width: 2px;
-        border-radius: 0;
-        border-bottom-left-radius: var(--wjb-border-radius);
-        border-bottom-right-radius: var(--wjb-border-radius);
-    }
-
-    &.border-right {
-        border-right-width: 2px;
-        border-radius: 0;
-        border-top-left-radius: var(--wjb-border-radius);
-        border-bottom-left-radius: var(--wjb-border-radius);
-    }
-
-    &.border-bottom {
-        border-bottom-width: 2px;
-        border-radius: 0;
-        border-top-left-radius: var(--wjb-border-radius);
-        border-top-right-radius: var(--wjb-border-radius);
-    }
-
-    &.border-left {
-        border-left-width: 2px;
-        border-radius: 0;
-        border-top-right-radius: var(--wjb-border-radius);
-        border-bottom-right-radius: var(--wjb-border-radius);
-    }
-
-    & > header {
-        margin: -1rem;
-        margin-bottom: 1rem;
-        background-color: var(--wjb-primary);
-        background: linear-gradient(
-            -5deg,
-            var(--wjb-primary-dark),
-            var(--wjb-primary),
-        );
-        color: var(--wjb-light);
-        text-shadow: 1px 1px rgba(0, 0, 0, 0.6);
-        box-shadow: 0 2px 12px rgba(0, 0, 0, 0.1), 0 6px 16px -12px rgba(0, 0, 0, 1);
-        border: 1px solid var(--wjb-primary-dark);
-        border-radius: var(--wjb-border-radius);
-
-        h3 {
-            line-height: 1em;
-            margin: 1rem;
         }
     }
 }
