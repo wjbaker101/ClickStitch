9af35551aa8754daa4d7060f99201656f7c8bfd8
William Baker <wjbaker101@users.noreply.github.com>
2024-06-01 10:30:18 +0100

    Use Tailwind

--- frontend/src/components/context-menu/ContextMenuSeparatorComponent.vue
+++ frontend/src/components/context-menu/ContextMenuSeparatorComponent.vue
@@ -1,13 +1,9 @@
 <template>
-    <div class="context-menu-separator-component"></div>
+    <div class="border-0 border-solid px-2 context-menu-separator-component border-b-[1px] border-primary-light"></div>
 </template>
 
 <script setup lang="ts">
 </script>
 
 <style lang="scss">
-.context-menu-separator-component {
-    margin: 0 0.5rem;
-    border-bottom: 1px solid var(--wjb-primary-light);
-}
 </style>
