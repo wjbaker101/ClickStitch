da657fa9bccaed1f39af2322952e5f9d49581514
William Baker <wjbaker101@users.noreply.github.com>
2024-06-02 19:02:47 +0100

    Use newly created utility class

--- frontend/src/views/stitcher/project/components/ActionBarComponent.vue
+++ frontend/src/views/stitcher/project/components/ActionBarComponent.vue
@@ -1,11 +1,11 @@
 <template>
-    <div class="absolute inset-0 top-auto flex items-center gap-4 p-1 text-center backdrop-blur-sm action-bar-component text-light [text-shadow:1px_1px_rgba(0,0,0,.6)] shadow-lg border-1 border-solid border-primary-dark rounded-t-md">
+    <div class="absolute inset-0 top-auto flex items-center gap-4 rounded-t-md border-solid p-1 text-center shadow-lg backdrop-blur-sm action-bar-component text-light text-shadow border-1 border-primary-dark">
         <div>
             <div>{{ percentageCompleted.toFixed(2) }}%</div>
         </div>
         <div class="centre-container">
             <div class="flex items-center centre-button" @click="onShowModal">
-                <IconComponent icon="info" />
+                <IconComponent icon="info" class="drop-shadow-icon" />
             </div>
         </div>
         <div>
