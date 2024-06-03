c5d142f174189752540b4eec30e567932906f59e
William Baker <wjbaker101@users.noreply.github.com>
2024-06-03 17:35:58 +0100

    Use Tailwind

--- frontend/src/views/_shared/login/components/ContentCardComponent.vue
+++ frontend/src/views/_shared/login/components/ContentCardComponent.vue
@@ -1,6 +1,11 @@
 <template>
-    <CardComponent padded class="relative content-card-component text-light">
-        <img width="90" height="90" src="@/assets/logo-text.jpg" class="absolute top-0 left-1/2 logo p-2 bg-[#efefef] rounded-full shadow-md border-2 border-solid border-primary -translate-x-1/2 -translate-y-1/2">
+    <CardComponent padded class="relative bg-gradient-to-tl content-card-component text-light from-primary-dark to-primary">
+        <img
+            width="90"
+            height="90"
+            src="@/assets/logo-text.jpg"
+            class="absolute top-0 left-1/2 logo p-2 bg-[#efefef] rounded-full shadow-md border-2 border-solid border-primary -translate-x-1/2 -translate-y-1/2"
+        >
         <slot></slot>
     </CardComponent>
 </template>
@@ -9,12 +14,4 @@
 </script>
 
 <style lang="scss">
-.content-card-component {
-    background-color: var(--wjb-primary);
-    background: linear-gradient(
-        -25deg,
-        var(--wjb-primary-dark),
-        var(--wjb-primary),
-    );
-}
 </style>
