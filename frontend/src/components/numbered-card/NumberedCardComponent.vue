12fe25875a7761bf817283961477acaba4f2364b
William Baker <wjbaker101@users.noreply.github.com>
2024-06-03 22:52:43 +0100

    Prefer scoped style over classname, it gets lost in all the Tailwind classes

--- frontend/src/components/numbered-card/NumberedCardComponent.vue
+++ frontend/src/components/numbered-card/NumberedCardComponent.vue
@@ -1,6 +1,6 @@
 <template>
-    <CardComponent padded border="top" class="relative numbered-card-component">
-        <div class="absolute top-0 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-full border-2 border-solid p-4 text-center font-bold number size-16 bg-background text-3xl/none text-secondary border-primary"></div>
+    <CardComponent padded border="top" class="relative">
+        <div class="absolute top-0 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-full border-2 border-solid p-4 text-center font-bold size-16 bg-background text-3xl/none text-secondary border-primary"></div>
         <slot></slot>
     </CardComponent>
 </template>
@@ -8,13 +8,11 @@
 <script setup lang="ts">
 </script>
 
-<style lang="scss">
-.numbered-card-component {
-    .number {
-        &::before {
-            counter-increment: index;
-            content: counter(index);
-        }
+<style lang="scss" scoped>
+* {
+    & > :first-child::before {
+        counter-increment: index;
+        content: counter(index);
     }
 }
 </style>
