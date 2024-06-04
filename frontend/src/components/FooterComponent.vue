852cc5e008391ac2723e038322868245a8fc70a9
William Baker <wjbaker101@users.noreply.github.com>
2024-06-04 18:00:12 +0100

    Remove usages of section so that custom styling can be removed for more consistent layouts

--- frontend/src/components/FooterComponent.vue
+++ frontend/src/components/FooterComponent.vue
@@ -1,24 +1,23 @@
 <template>
     <footer class="p-2 text-xs/loose">
-        <section>
+        <div class="mb-4">
             <div class="inline-block">Links:</div>
-            <div class="ml-2 inline-block"><LinkComponent href="/dashboard">Dashboard</LinkComponent></div>
-            <div class="ml-2 inline-block"><LinkComponent href="/patterns">Patterns</LinkComponent></div>
-            <div class="ml-2 inline-block"><LinkComponent href="/inventory">Inventory</LinkComponent></div>
-            <div class="ml-2 inline-block"><LinkComponent href="/about">About</LinkComponent></div>
-            <div class="ml-2 inline-block"><LinkComponent href="/settings">Settings</LinkComponent></div>
+            <LinkComponent href="/dashboard" class="ml-2">Dashboard</LinkComponent>
+            <LinkComponent href="/patterns" class="ml-2">Patterns</LinkComponent>
+            <LinkComponent href="/inventory" class="ml-2">Inventory</LinkComponent>
+            <LinkComponent href="/about" class="ml-2">About</LinkComponent>
+            <LinkComponent href="/settings" class="ml-2">Settings</LinkComponent>
             <br>
             <div class="inline-block">Follow on:</div>
-            <div class="ml-2 inline-block"><LinkComponent :href="lemmyUrl" external>Lemmy</LinkComponent></div>
-            <div class="ml-2 inline-block"><LinkComponent :href="instagramUrl" external>Instagram</LinkComponent></div>
-            <div class="ml-2 inline-block"><LinkComponent :href="twitterUrl" external>Twitter</LinkComponent></div>
-        </section>
-        <section>
+            <LinkComponent :href="lemmyUrl" external class="ml-2">Lemmy</LinkComponent>
+            <LinkComponent :href="instagramUrl" external class="ml-2">Instagram</LinkComponent>
+            <LinkComponent :href="twitterUrl" external class="ml-2">Twitter</LinkComponent>
+        </div>
+        <div>
             Copyright &copy; ClickStitch 2023 &ndash; {{ new Date().getFullYear() }},
             <br>
             All rights reserved.
-        </section>
-        <div>
+            <br>
             Icons by <LinkComponent href="https://akveo.github.io/eva-icons/#/" external>Eva icons</LinkComponent>, fonts by <LinkComponent href="https://fonts.google.com/" external>Google Fonts</LinkComponent>.
         </div>
     </footer>
