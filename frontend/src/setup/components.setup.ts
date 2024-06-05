8787f254ae91911713115acf77ea87a7c96b2924
William Baker <wjbaker101@users.noreply.github.com>
2024-06-05 19:22:13 +0100

    Now unused

--- frontend/src/setup/components.setup.ts
+++ frontend/src/setup/components.setup.ts
@@ -1,7 +1,5 @@
 import { type Plugin } from 'vue';
 
-import IconComponent from '@wjb/vue/component/IconComponent.vue';
-
 import CardComponent from '@/components/CardComponent.vue';
 import LinkComponent from '@/components/LinkComponent.vue';
 import ViewComponent from '@/components/ViewComponent.vue';
@@ -9,8 +7,6 @@ import ViewComponent from '@/components/ViewComponent.vue';
 export const components: Plugin = {
 
     install(app) {
-        app.component('IconComponent', IconComponent);
-
         app.component('CardComponent', CardComponent);
         app.component('LinkComponent', LinkComponent);
         app.component('ViewComponent', ViewComponent);
