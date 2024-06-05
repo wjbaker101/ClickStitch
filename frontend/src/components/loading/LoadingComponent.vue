bade38431ff9acbde4e29876bf48410cc60f5349
William Baker <wjbaker101@users.noreply.github.com>
2024-06-05 23:38:46 +0100

    Set margin to override the reset done by Tailwind's preflight

--- frontend/src/components/loading/LoadingComponent.vue
+++ frontend/src/components/loading/LoadingComponent.vue
@@ -6,7 +6,7 @@
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 100 100"
             preserveAspectRatio="xMidYMid"
-            class="max-w-full align-middle text-primary-dark animate-loading"
+            class="max-w-full align-middle text-primary-dark animate-loading mx-auto"
         >
             <circle cx="50" cy="50" fill="none" stroke="currentColor" stroke-width="5" r="47" stroke-dasharray="115 80" stroke-linecap="round">
                 <animateTransform attributeName="transform" type="rotate" repeatCount="indefinite" dur="1s" values="0 50 50;360 50 50" keyTimes="0;1" />
