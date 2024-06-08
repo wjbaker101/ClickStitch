ca742cb70f361908f553fff601f6d38816b60ddc
William Baker <wjbaker101@users.noreply.github.com>
2024-06-08 13:37:54 +0100

    Descriptions have been set, so this can be not nullable

---
+++ Migrations/Migrations/Migrations/0059 - SetDescriptionColumnOnCreatorTableAsNotNullable.cs
@@ -0,0 +1,19 @@
+﻿using FluentMigrator;
+using Migrations.Common;
+
+namespace Migrations.Migrations;
+
+// ReSharper disable once InconsistentNaming
+[Migration(0059)]
+public sealed class SetDescriptionColumnOnCreatorTableAsNotNullable_0059 : AutoReversingMigration
+{
+    public override void Up()
+    {
+        Alter
+            .Table(Names.Tables.CREATOR)
+            .InSchema(Names.Schemas.CLICK_STITCH)
+            .AlterColumn("description")
+            .AsString()
+            .NotNullable();
+    }
+}
