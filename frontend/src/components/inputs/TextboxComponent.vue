760a2ce2ba5e20c7968930fb85628437725de81b
William Baker <wjbaker101@users.noreply.github.com>
2024-06-08 13:02:55 +0100

    Create component for textarea for consistent styling

---
+++ frontend/src/components/inputs/TextboxComponent.vue
@@ -0,0 +1,14 @@
+<template>
+    <textarea
+        v-model="value"
+        class="w-full rounded-md px-4 py-2 shadow-md outline-dashed outline-2 outline-transparent transition-[outline-color] bg-background-light min-h-24 focus:outline-primary"
+    >
+    </textarea>
+</template>
+
+<script setup lang="ts">
+const value = defineModel<string>();
+</script>
+
+<style lang="scss">
+</style>
